e89fedf249f2dda823bef41abbd8e2c1849ab611
sergiioss <sergiiptx@gmail.com>
2023-05-30 20:17:04 +0200

    Agrego dependencia de boostrap y empiezo el header

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Inicio from './containers/Inicio/Inicio'
+import FitnessTraining from './containers/Training/Fitness/FitnessTraining'
+import HitTraining from './containers/Training/Hit/HitTraining'
+import BeginnersTraining from './containers/Training/Beginners/BeginnersTraining'
+import AerobicExercisesTraining from './containers/Training/AerobicExercise/AerobicExercisesTraining'
+import BigBodybuildersTraining from './containers/Training/BigBodybuilders/BigBodybuildersTraining'
+import StretchingTraining from './containers/Training/Stretching/StretchingTraining'
 
 function App() {
   
@@ -11,6 +17,54 @@ function App() {
    <BrowserRouter>
     <Header/>
       <Routes>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/musculacion" element={<FitnessTraining/>}></Route>
+        <Route path="/entrenamiento-alta-intensidad" element={<HitTraining/>}></Route>
+        <Route path="/principiantes" element={<BeginnersTraining/>}></Route>
+        <Route path="/ejercicio-aerobico" element={<AerobicExercisesTraining/>}></Route>
+        <Route path="/culturistas" element={<BigBodybuildersTraining/>}></Route>
+        <Route path="/estiramientos" element={<StretchingTraining/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio/>}></Route>
         <Route path="/" element={<Inicio/>}></Route>
         <Route path="/" element={<Inicio/>}></Route>
         <Route path="/" element={<Inicio/>}></Route>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import './Header.css'
-
+import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
 
 const Header = () => {
 
     return (
-
-        <div className="header">
-            <div>
-                Header
-            </div>
-        </div>
+        <Container className="header">
+            <Link className="icon-facebook" to="https://www.facebook.com/CambiatufisicoWeb/"></Link>
+            <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
+            <Link className="icon-twitter" to="https://twitter.com/cambiatufisico"></Link>
+            <Link className="icon-youtube" to="https://www.youtube.com/c/Cambiatufisicoweb"></Link>
+        </Container>
     )
 }
 
