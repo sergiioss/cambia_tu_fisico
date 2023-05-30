475a2e0f8448a0292ebbf6fc5f08c8b61f4f5f46
sergiioss <sergiiptx@gmail.com>
2023-05-30 22:09:53 +0200

    Imagen header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,24 @@
 import './Header.css'
 import { Link } from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import logoPagina from '../../Assets/images/logoPagina.jpg';
 
 const Header = () => {
 
     return (
         <Container className="header">
-            <Link className="icon-facebook" to="https://www.facebook.com/CambiatufisicoWeb/"></Link>
-            <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
-            <Link className="icon-twitter" to="https://twitter.com/cambiatufisico"></Link>
-            <Link className="icon-youtube" to="https://www.youtube.com/c/Cambiatufisicoweb"></Link>
+            <Row className="header-links">
+                <Link className="icon-facebook" to="https://www.facebook.com/CambiatufisicoWeb/"></Link>
+                <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
+                <Link className="icon-twitter" to="https://twitter.com/cambiatufisico"></Link>
+                <Link className="icon-youtube" to="https://www.youtube.com/c/Cambiatufisicoweb"></Link>
+            </Row>
+            <Row className="header-img">
+                <Image src={logoPagina}></Image>
+            </Row>
         </Container>
     )
 }
