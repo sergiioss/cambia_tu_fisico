7a765e9230bc6f1016bec9bc809af934226f6bf4
sergiioss <sergiiptx@gmail.com>
2023-06-16 00:22:53 +0200

    4 apartados mas para movil

--- src/containers/Training/Beginners/BeginnersTraining.jsx
+++ src/containers/Training/Beginners/BeginnersTraining.jsx
@@ -12,47 +12,93 @@ const BeginnersTraining = () => {
         <Container className="beginners-exercises-training">
             <Row className="first-section-beginners">
                 <Col className="body-beginners-training" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
-                    <h1>Musculación, la guía definitiva</h1>
+                    <h1>Guía de Gimnasio y Musculación para principiantes</h1>
                     <i>Con nuestros consejos y técnicas dominarás el arte de la musculación para cambiar tu cuerpo.</i>
                     <Row className="icons-beginners" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
                         <Link className="icon-facebook" to="https://www.facebook.com/CambiatufisicoWeb/"></Link>
                         <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
                         <Link className="icon-twitter" to="https://twitter.com/cambiatufisico"></Link>
                     </Row>
-                    <Image className="img-beginners" src="https://www.cambiatufisico.com/wp-content/uploads/musculacion5-696x392.jpg"></Image>
+                    <Image className="img-beginners" src="https://www.cambiatufisico.com/wp-content/uploads/guia-musculacion-696x392.jpg"></Image>
                     <section>
                         <p>
-                            La <strong>musculación</strong> es una disciplina que requiere tiempo, dedicación y esfuerzo. Como su propio nombre indica, su objetivo es el <strong>desarrollo de los músculos.</strong>
+                            El primer día que entres en la sala de musculación te encontrarás en un territorio desconocido, no sabrás dónde están los materiales, te sentirás observado y tendrás miedo a realizar los ejercicios.
                         </p>
                         <p>
-                            En la actualidad, la práctica de la <strong>musculación</strong> es uno de los recursos más utilizados por todo tipo de personas ya sea para mejorar su físico, <a className="links-beginners" href="">ganar masa muscular</a>, <a className="links-beginners">perder peso</a> o simplemente como complemento para todo tipo de deportes.
+                            Los motivos por los que las personas se apuntan por primera vez al gimnasio u otra actividad física son tan diferentes como las propias personas.
                         </p>
                         <p>
-                            Te adelantamos que el éxito del proceso de musculación depende de tres pilares básicos: La dieta, el entrenamiento con rutinas de gimnasio adecuadas y el descanso. Si cuidas estos tres pilares tu éxito a la hora de alcanzar tus objetivos está asegurado.
+                            Quizás quieras ganar más fuerza, ser más resistente o, como la mayoría, resultar más atractivo/a para el sexo opuesto mientras mejoras tu salud.
                         </p>
                         <Row className="square-beginners">
                             <p className="title-square-beginners">Contenidos</p>
                             <ul>
-                                <li className="li-square-beginners">1 Los tres principios de la musculación</li>
-                                <li>2 Musculación por grupos musculares</li>
-                                <li>3 Selección de artículos de musculación</li>
+                                <li className="li-square-beginners">1 Antes de empezar</li>
+                                <li>2 Tu primer día en el gimnasio</li>
+                                <li>3 Tus primeras sesiones en el gimnasio</li>
+                                <li>4 Conceptos básicos de musculación para principiantes</li>
+                                <li>5 El entrenamiento del principiante</li>
+                                <li>6 Alimentación del principiante</li>
+                                <li>7 Suplementos alimenticios</li>
+                                <li>8 Todo lo que debes llevar al gym</li>
+                                <li>9 Pautas básicas de comportamiento en el gimnasio</li>
+                                <li>10 Pautas a seguir en las primeras rutinas</li>
+                                <li>11 Errores más comunes del principiante</li>
+                                <li>12 Criterios básicos de seguridad</li>
+                                <li>13 Se principiante, no estúpido</li>
                             </ul>
                         </Row>
-                        <h3>Los tres principios de la musculación</h3>
+                        <h3>Antes de empezar</h3>
                         <p>
-                            En el entrenamiento de la fuerza y la masa muscular hay tres principios de entrenamiento especialmente importantes y que toda persona que inicia en las pesas debe conocer.
+                            No importa cual sea tu <strong>motivación</strong>, lo importante es que lo has decidido, has dado el paso, has tomado la decisión correcta.
                         </p>
                         <p>
-                            Estos tres principios se basan en el orden correcto al realizar los ejercicios, progresión de entrenamiento y recuperación adecuada.
-                        </p>
-                        <h5>Orden de los ejercicios</h5>
-                        <p>
-                            Según este principio, los músculos grandes deben ser ejercitados antes que los pequeños. La razón es muy simple: un músculo pequeño se fatiga antes y con mayor facilidad que uno grande.
-                        </p>
-                        <p>
-                            Por consiguiente, si los músculos pequeños son ejercitados al principio de la sesión, pueden provocar una fatiga prematura general, impidiendo luego el óptimo rendimiento de los grupos musculares grandes.
+                            PERO TEN CUIDADO, MUCHAS VECES EL DESCONOCIMIENTO Y LAS «PRISAS» PUEDEN DAR AL TRASTE CON TUS OBJETIVOS.
                         </p>
                     </section>
+                        <Image className="img-beginners" src="https://www.cambiatufisico.com/wp-content/uploads/musculacion-novatos.jpg"></Image>
+                        <p>
+                            El Fisicoculturismo nos permite ganar masa muscular con más facilidad que cualquier otra disciplina, a la vez que nos confiere un físico que despierta admiración, pero sólo resulta cuando se lleva un programa claramente definido. Así que te enseñaremos y te guiaremos durante tus primero tres meses de entrenamiento.
+                        </p>
+                        <p>
+                            <strong>Lee con mucha atención esta guía; antes de empezar debes hacer caso a todos estos puntos:</strong>
+                        </p>
+                        <p>
+                            Pasa un examen médico si tienes más de 40 años, si tienes lesiones o algún problema de salud.
+                        </p>
+                        <p>
+                            Plantéate objetivos realistas, ten en cuenta tu condición actual y dónde quieres estar en tres meses, un año o cinco y apégate a ello.
+                        </p>
+                        <p>
+                            Espera hasta que transcurran tres meses antes de juzgar tus progresos. Ten paciencia, construir un buen cuerpo lleva tiempo; ya lo sabes: Roma no se hizo en un día.
+                        </p>
+                        <p>
+                            El entrenamiento en el gimnasio constituye la mitad del progreso. Dormir, descansar bien y consumir los alimentos y suplementos adecuados son tanto o más importantes para el desarrollo y formación muscular.
+                        </p>
+                        <p>
+                            Bien, pues ya estamos listos para empezar.
+                        </p>
+                        <h5>Tu primer día en el gimnasio</h5>
+                        <p>
+                            Ya has tomado la decisión y vas a empezar tu primer día en el gimnasio.
+                        </p>
+                        <p>
+                            Después de haber visitado algunos gimnasios y conocido las opiniones de amigos y familiares que ya entrenan, decidimos apuntarnos a uno, que puede o no estar cerca de nuestro hogar o punto de trabajo -es preferible que lo esté: la proximidad evita la tentación del abandono-, después de haber elegido las clases y horarios adecuados y contratado incluso los servicios de un entrenador personal.
+                        </p>
+                        <h5>¡Me da vergüenza…!</h5>
+                        <p>
+                            Uno de los problemas <strong>psicológicos</strong> con los que suele tener que enfrentarse el novato de cualquier edad es el de la presunta recepción que cree van a hacerle los compañeros de ejercicio.
+                        </p>
+                        <p> 
+                            Muchas personas indecisas o inseguras, temen encontrarse en los gimnasios con verdaderos
+                            atletas de ambos sexos, altos, esbeltos, bien parecidos, muy desarrollados y despectivos, y dudan antes de atreverse a entrar
+                        </p>
+                        <p>
+                            Piensan, de forma equivocada, que van a reírse de ellos, les mirarán de reojo juzgando lo que hacen, etc… sobre todo si el novato es de avanzada edad, no está en forma o tiene un físico desproporcionado.
+                        </p>
+                        <p>
+                            Se trata de un grave error de apreciación, porque las cosas jamás suceden allí de esa manera.
+                        </p>
                 </Col>
                 <Aside className="aside"/>
             </Row>
--- src/containers/Training/Hit/HitTraining.jsx
+++ src/containers/Training/Hit/HitTraining.jsx
@@ -12,7 +12,7 @@ const HitTraining = () => {
         <Container className="hit-exercises-training">
         <Row className="first-section-hit">
             <Col className="body-hit-training" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
-                <h1>Musculación, la guía definitiva</h1>
+                <h1>Entrenamiento de alta intensidad | Consejos y Técnicas</h1>
                 <i>Con nuestros consejos y técnicas dominarás el arte de la musculación para cambiar tu cuerpo.</i>
                 <Row className="icons-hit" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
                     <Link className="icon-facebook" to="https://www.facebook.com/CambiatufisicoWeb/"></Link>
@@ -22,37 +22,90 @@ const HitTraining = () => {
                 <Image className="img-hit" src="https://www.cambiatufisico.com/wp-content/uploads/alta-intesidad-696x392.jpg"></Image>
                 <section>
                     <p>
-                        La <strong>musculación</strong> es una disciplina que requiere tiempo, dedicación y esfuerzo. Como su propio nombre indica, su objetivo es el <strong>desarrollo de los músculos.</strong>
+                        Las técnicas de entrenamiento de alta intensidad supondrán un nuevo reto para tu físico.
                     </p>
                     <p>
-                        En la actualidad, la práctica de la <strong>musculación</strong> es uno de los recursos más utilizados por todo tipo de personas ya sea para mejorar su físico, <a className="links-hit" href="">ganar masa muscular</a>, <a className="links-hit">perder peso</a> o simplemente como complemento para todo tipo de deportes.
+                        Después de un tiempo entrenando y tener muy claros los conceptos de musculación es muy posible que tu físico haya experimentado un cambio bastante notable.
                     </p>
                     <p>
-                        Te adelantamos que el éxito del proceso de musculación depende de tres pilares básicos: La dieta, el entrenamiento con rutinas de gimnasio adecuadas y el descanso. Si cuidas estos tres pilares tu éxito a la hora de alcanzar tus objetivos está asegurado.
+                        Ahora ha llegado el momento de alcanzar un nuevo nivel de <strong>crecimiento muscular</strong> añadiendo <strong>técnicas culturistas avanzadas</strong> a tus rutinas de gimnasio.
+                    </p>
+                    <p>
+                        <strong>Recuerda que en esta sección nos centramos en técnicas de alta intensidad pero tienes mucha más información sobre otra técnicas de entrenamiento en nuestra sección de musculación.</strong>
                     </p>
                     <Row className="square-hit">
                         <p className="title-square-hit">Contenidos</p>
                         <ul>
-                            <li className="li-square-hit">1 Los tres principios de la musculación</li>
-                            <li>2 Musculación por grupos musculares</li>
-                            <li>3 Selección de artículos de musculación</li>
+                            <li className="li-square-hit">1 Intensidad del entrenamiento</li>
+                            <li>2 Intensidad y fallo muscular</li>
+                            <li>3 Ventajas del entrenamiento de alta intensidad</li>
+                            <li>4 Reglas para el entrenamiento de alta intensidad</li>
+                            <li>5 Métodos para aumentar la intensidad de entrenamiento</li>
+                            <li>6 Todas las técnicas de gimnasio de alta intensidad</li>
+                            <li>7 No abuses de las técnicas de alta intensidad</li>
                         </ul>
                     </Row>
-                    <h3>Los tres principios de la musculación</h3>
+                    <h5>Intensidad del entrenamiento</h5>
                     <p>
-                        En el entrenamiento de la fuerza y la masa muscular hay tres principios de entrenamiento especialmente importantes y que toda persona que inicia en las pesas debe conocer.
+                        El término intensidad se emplea en varios contextos cuando se refiere al levantamiento de peso.
                     </p>
                     <p>
-                        Estos tres principios se basan en el orden correcto al realizar los ejercicios, progresión de entrenamiento y recuperación adecuada.
-                    </p>
-                    <h5>Orden de los ejercicios</h5>
-                    <p>
-                        Según este principio, los músculos grandes deben ser ejercitados antes que los pequeños. La razón es muy simple: un músculo pequeño se fatiga antes y con mayor facilidad que uno grande.
+                        En nuestras rutinas siempre incidimos que lo importante no es machacar el músculo con un número interminable de series sino aplicar una intensidad adecuada de entrenamiento, con un número de series moderado.
                     </p>
                     <p>
-                        Por consiguiente, si los músculos pequeños son ejercitados al principio de la sesión, pueden provocar una fatiga prematura general, impidiendo luego el óptimo rendimiento de los grupos musculares grandes.
+                        Muchos usan la palabra intensidad simplemente para expresar cuánto peso se está utilizando.
+                    </p>
+                    <p>
+                        Pero esto es sólo la mitad de la cuestión. La intensidad es en realidad la cantidad de peso que se utiliza en una serie dada, además del número de repeticiones que se realizan con dicho peso y con una concentración mental máxima en la ejecución del ejercicio.
+                    </p>
+                    <p>
+                        La intensidad de una serie es relativa para cada persona; una serie de diez repeticiones con 45 kilos será muy intensa para algunos pero muy fácil (o no tan intensa) para otros.
+                    </p>
+                    <p>
+                        Por eso nunca contesto los comentarios en plan «En el ejercicio de curl con mancuernas ¿de cuanto peso cojo las mancuernas?». Amigo, solo uno mismo puede saber la cantidad de intensidad que puede soportar, hay que ir probando.
+                    </p>
+                    <h5>Intensidad y fallo muscular</h5>
+                    <p>
+                        Para simplificar más las cosas, la intensidad puede ser equiparada con el concepto de entrenamiento al fallo. Esto significa hacer una serie con un peso dado hasta el punto en el cual una repetición más con dicho peso sea físicamente imposible manteniendo un estilo correcto de levantamiento.
+                    </p>
+                    <p>
+                        Por ejemplo, si el número de repeticiones que puedes hacer con 45 kilos es de 10 y haces 10 repeticiones, entrenarás hasta el fallo en esta serie; si paras después de la octava repetición, no estarás entrenando al fallo.
+                    </p>
+                    <p>
+                        La intensidad no es simplemente una cuestión de cuánto peso utilizas o cuántas repeticiones haces, sino también de CUÁNDO entrenas hasta el fallo.
+                    </p>
+                    <p>
+                        Una serie de 20 repeticiones puede ser tan intensa como una serie de 6; aunque utilices pesos significativamente más ligeros, podrás llegar al fallo (no serás capaz de hacer la repetición número 21). Las dos series son muy intensas porque has llegado al fallo muscular.
+                    </p>
+                    <h5>Ventajas del entrenamiento de alta intensidad</h5>
+                    <p>
+                        Estas técnicas de entrenamiento de alta intensidad supondrán un nuevo reto para tu cuerpo, que tendrá que adaptarse o morir en el intento. Además es necesario que empieces a utilizar la cabeza y no solo los músculos en tus entrenamientos.
                     </p>
                 </section>
+                    <Image className="img-hit" src="https://www.cambiatufisico.com/wp-content/uploads/entrenamiento-avanzado.jpg"></Image>
+                    <p>
+                        Para ello es imprescindible una <strong>planificación</strong> de los entrenamientos, de los descansos, de las variaciones de los ejercicios, tener en cuenta los tiempos de ejecución, etc.
+                    </p>
+                    <p>
+                        Utiliza estas técnicas en el gimnasio de forma planificada, sin abusar en exceso de ellas. En caso contrario puedes caer en el temido sobreentrenamiento o lo que es peor, en un <strong>estancamiento</strong> tanto anímico como físico que te impedirá alcanzar tus objetivos culturistas.
+                    </p>
+                    <p>
+                        Para entrenar con alta intensidad hay que seguir unos criterios adecuados. De otro modo puedes lesionarte o caer en el sobreentrenamiento.
+                    </p>
+                    <h3>Reglas para el entrenamiento de alta intensidad</h3>
+                    <p>
+                        Una típica serie de trabajo es prolongada y extenuante, pero aquí, cuando el <strong>músculo</strong> falla, la serie está justo empezando. Aquí van unos consejos para ayudarte a conseguir las recompensas del entrenamiento de alta intensidad.
+                    </p>
+                    <h5>Estrategia</h5>
+                    <p>
+                        Las repeticiones forzadas y las repeticiones negativas utilizando la octava repetición máxima son las técnicas de mayores exigencias. Ir más allá del fallo con el peso máximo posible es el mayor estímulo para ganar masa muscular y fuerza.
+                    </p>
+                    <p>
+                        No obstante, estas técnicas extremas exigen un alto nivel de precaución, por lo que debes de tener la ayuda de un compañero de entrenamiento.
+                    </p>
+                    <p>
+                        Llevar el peso máximo más allá del fallo, con repeticiones forzadas y repeticiones negativas, debe utilizarse de forma moderada. Mientras se realizan estas maniobras, existe una delgada línea entre el estímulo muscular, el sobreentrenamiento y la lesión.
+                    </p>
             </Col>
             <Aside className="aside"/>
         </Row>
