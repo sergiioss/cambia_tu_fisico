e6d9bae2775a29ab5d8ca4a52abd30970f5478c7
sergiioss <sergiiptx@gmail.com>
2023-06-19 19:37:22 +0200

    abdominal

--- src/containers/ForMuscle/Abdominal/AbdominalTraining.jsx
+++ src/containers/ForMuscle/Abdominal/AbdominalTraining.jsx
@@ -9,50 +9,94 @@ const AbdominalTraining = () => {
 
     return (
 
-        <Container className="fitness-training">
-            <Row className="first-section-fitness">
-                <Col className="body-fitness-training" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
-                    <h1>Musculación, la guía definitiva</h1>
-                    <i>Con nuestros consejos y técnicas dominarás el arte de la musculación para cambiar tu cuerpo.</i>
-                    <Row className="icons-fitness" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
+        <Container className="abdominal-training">
+            <Row className="first-section-abdominal">
+                <Col className="body-abdominal-training" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
+                    <h1>Entrenamiento de los Abdominales, como conseguir tu six-pack</h1>
+                    <br />
+                    <Row className="icons-abdominal" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
                         <Link className="icon-facebook" to="https://www.facebook.com/CambiatufisicoWeb/"></Link>
                         <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
                         <Link className="icon-twitter" to="https://twitter.com/cambiatufisico"></Link>
                     </Row>
-                    <Image className="img-fitness" src="https://www.cambiatufisico.com/wp-content/uploads/musculacion5-696x392.jpg"></Image>
+                    <Image className="img-abdominal" src="https://www.cambiatufisico.com/wp-content/uploads/abdominales5-696x392.jpg"></Image>
                     <section>
                         <p>
-                            La <strong>musculación</strong> es una disciplina que requiere tiempo, dedicación y esfuerzo. Como su propio nombre indica, su objetivo es el <strong>desarrollo de los músculos.</strong>
+                            Si vas al gimnasio es muy posible que tu objetivo sea ganar masa muscular y perder grasa. ¿De verdad sabes lo importante que es entrenar tus abdominales para conseguir el éxito?
                         </p>
                         <p>
-                            En la actualidad, la práctica de la <strong>musculación</strong> es uno de los recursos más utilizados por todo tipo de personas ya sea para mejorar su físico, <a className="links-fitness" href="">ganar masa muscular</a>, <a className="links-fitness">perder peso</a> o simplemente como complemento para todo tipo de deportes.
+                            Va mucho más allá de conseguir lucir six-pack en la playa para impresionar a las chicas; el abdomen es vital para nuestra postura, nuestra fuerza y nuestras habilidades atléticas.
                         </p>
                         <p>
-                            Te adelantamos que el éxito del proceso de musculación depende de tres pilares básicos: La dieta, el entrenamiento con rutinas de gimnasio adecuadas y el descanso. Si cuidas estos tres pilares tu éxito a la hora de alcanzar tus objetivos está asegurado.
+                            Para realizar un correcto entrenamiento de los abdominales  es necesario saber como funciona este complejo grupo muscular.
                         </p>
-                        <Row className="square-fitness">
-                            <p className="title-square-fitness">Contenidos</p>
+                        <p>
+                            Por lo tanto, antes de empezar cualquier rutina de abdominales te recomendamos que leas con atención las siguientes líneas sobre entrenamiento abdominal.
+                        </p>
+                        <Row className="box-abdominal-parrafo">
+                            <p className="abdominal-training-square">
+                                Una vez conozcas los principios del entrenamiento abdominal, revisa nuestra sección de <br /><strong className="title-red-abdominal-square">ejercicios de abdominales.</strong>
+                            </p>
+                        </Row>
+                        <Row className="square-abdominal">
+                            <p className="title-square-abdominal">Contenidos</p>
                             <ul>
-                                <li className="li-square-fitness">1 Los tres principios de la musculación</li>
-                                <li>2 Musculación por grupos musculares</li>
-                                <li>3 Selección de artículos de musculación</li>
+                                <li className="li-square-abdominal">1 Para qué sirve nuestro abdomen</li>
+                                <li>2 Orden de trabajo del abdomen</li>
+                                <li>3 Como entrenar el abdomen para máximos resultados</li>
+                                <li>4 Otros artículos de entrenamiento abdominal</li>
                             </ul>
                         </Row>
-                        <h3>Los tres principios de la musculación</h3>
+                        <h3>Para qué sirve nuestro abdomen</h3>
                         <p>
-                            En el entrenamiento de la fuerza y la masa muscular hay tres principios de entrenamiento especialmente importantes y que toda persona que inicia en las pesas debe conocer.
+                            La musculatura abdominal suele tener un carácter fásico, es decir, suele estar con falta de tono muscular.
                         </p>
                         <p>
-                            Estos tres principios se basan en el orden correcto al realizar los ejercicios, progresión de entrenamiento y recuperación adecuada.
-                        </p>
-                        <h5>Orden de los ejercicios</h5>
-                        <p>
-                            Según este principio, los músculos grandes deben ser ejercitados antes que los pequeños. La razón es muy simple: un músculo pequeño se fatiga antes y con mayor facilidad que uno grande.
+                            Esto es debido a que no se ve solicitado en nuestra vida diaria y no se involucra en acciones musculares, sobre todo por los trabajos sedentarios.
                         </p>
                         <p>
-                            Por consiguiente, si los músculos pequeños son ejercitados al principio de la sesión, pueden provocar una fatiga prematura general, impidiendo luego el óptimo rendimiento de los grupos musculares grandes.
+                            Si a esto le añadimos que su antagonista (musculatura lumbar) suele tener un excesivo tono, nos conlleva a una descompensación en la región pélvica y lumbar que desembocará en problemas posturales como; anteversión y antepulsión de la cadera, hiperlordosis, problemas de ciáticas, hernias discales y sobrecargas lumbares.
                         </p>
                     </section>
+                    <Image className="img-abdominal" src="https://www.cambiatufisico.com/wp-content/uploads/Entrenar-abdominales-3.jpg"></Image>
+                    <br />
+                    <h5>Actúa como fijador y estabilizador del tronco en un gran número de movimientos</h5>
+                    <p>
+                        La musculatura del tronco representa el centro de tu fuerza y de tu poder físico. En esfuerzos donde levantamos y empujamos resistencias, mantiene la columna como una base sólida, permitiendo transferir toda tu fuerza hacia los brazos y piernas para que realicen el esfuerzo.
+                    </p>
+                    <h5>Actúa a modo de «faja natural», interviniendo en el mantenimiento de la postura</h5>
+                    <p>
+                        Mantiene una alineación corporal correcta. Esto conlleva una serie de beneficios entre los que destacan una mayor efectividad del ejercicio, aumentando el rendimiento y disminuyendo el riesgo de lesión además de proporcionar estabilidad a la columna dorso-lumbar y mantener a las vísceras abdominales en su sitio.
+                    </p>
+                    <h5>Evitan descompensaciones musculares</h5>
+                    <p>
+                        En la mayoría de la población la zona posterior del tronco (lumbar) está con exceso de tono, ya que son músculos que intervienen en cualquier gesto y en el mantenimiento de la postura, mientras que la zona del abdomen esta con falta de tono.
+                    </p>
+                    <p>
+                        Esta situación provoca descompensaciones musculares apareciendo patologías de tipo degenerativo de la columna como lordosis, escoliosis, etc.
+                    </p>
+                    <h5>Protección órganos internos</h5>
+                    <p>
+                        Los músculos del abdomen sostienen y protegen los órganos internos. Es la única defensa en esta zona, ya que está desprovista de estructura ósea.
+                    </p>
+                    <p>
+                        Por tanto, es indispensable tener unos fuertes abdominales en deportistas que tienen que proteger esta zona de impactos como boxeadores, artes marciales, etc.
+                    </p>
+                    <h5>Intervienen en la respiración y presión intraabdominal</h5>
+                    <p>
+                        Cuando hacemos una espiración profunda (soltar el aire), los músculos respiratorios y abdominales actúan ejerciendo la presión necesaria para expulsar el aire de los pulmones
+                    </p>
+                    <h5>Mejoran la figura</h5>
+                    <p>
+                        Sin duda la zona media es la parte del cuerpo que mas ayuda a realzar la figura, podemos tener unos brazos fuertes o unas piernas esbeltas, pero de nada sirve si tenemos esa «tripita» o un vientre abultado y sin forma.
+                    </p>
+                    <h5>Símbolo de buena forma</h5>
+                    <p>
+                        Al igual que siempre se ha identificado al bíceps como un símbolo de fuerza, se identifica a la zona media como el reflejo de un buen estado de forma.
+                    </p>
+                    <p>
+                        Si se tienen unos buenos abdominales y bien definidos, no cabe duda de que el resto del cuerpo estará a la altura.
+                    </p>
                 </Col>
                 <Aside className="aside"/>  
             </Row>
