7fa3c0e5182a842a2e162680295e42b3e42a7fd0
sergiioss <sergiiptx@gmail.com>
2023-06-19 20:15:58 +0200

    Espalda

--- src/containers/ForMuscle/Back/BackTraining.jsx
+++ src/containers/ForMuscle/Back/BackTraining.jsx
@@ -12,46 +12,213 @@ const BackTraining = () => {
         <Container className="fitness-training">
             <Row className="first-section-fitness">
                 <Col className="body-fitness-training" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
-                    <h1>Musculación, la guía definitiva</h1>
-                    <i>Con nuestros consejos y técnicas dominarás el arte de la musculación para cambiar tu cuerpo.</i>
+                    <h1>Rutinas y entrenamiento de la espalda</h1>
+                    <br />
                     <Row className="icons-fitness" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
                         <Link className="icon-facebook" to="https://www.facebook.com/CambiatufisicoWeb/"></Link>
                         <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
                         <Link className="icon-twitter" to="https://twitter.com/cambiatufisico"></Link>
                     </Row>
-                    <Image className="img-fitness" src="https://www.cambiatufisico.com/wp-content/uploads/musculacion5-696x392.jpg"></Image>
+                    <Image className="img-fitness" src="https://www.cambiatufisico.com/wp-content/uploads/entrenamiento-espalda1-696x392.jpg"></Image>
                     <section>
                         <p>
-                            La <strong>musculación</strong> es una disciplina que requiere tiempo, dedicación y esfuerzo. Como su propio nombre indica, su objetivo es el <strong>desarrollo de los músculos.</strong>
+                            Las rutinas de espalda son los entrenamientos más extenuantes con permiso del día de piernas. La espalda es un grupo muscular grande y requiere de un gran esfuerzo para una total movilización y trabajo de sus músculos
                         </p>
                         <p>
-                            En la actualidad, la práctica de la <strong>musculación</strong> es uno de los recursos más utilizados por todo tipo de personas ya sea para mejorar su físico, <a className="links-fitness" href="">ganar masa muscular</a>, <a className="links-fitness">perder peso</a> o simplemente como complemento para todo tipo de deportes.
+                            Tener una espalda amplia y musculada otorgará a tu físico esa proporción de «línea V» tan buscada por los culturistas y las personas aficionadas al fitness. El culturismo se basa en las proporciones, y una espalda estrecha hará que el resto de tu físico no parezca a la altura.
                         </p>
                         <p>
-                            Te adelantamos que el éxito del proceso de musculación depende de tres pilares básicos: La dieta, el entrenamiento con rutinas de gimnasio adecuadas y el descanso. Si cuidas estos tres pilares tu éxito a la hora de alcanzar tus objetivos está asegurado.
+                            Para que las rutinas de espalda sean efectivas necesitas una buena planificación y variación en las mismas, con los mejores ejercicios de espalda disponibles en tu arsenal. Si buscas rutinas de espalda efectivas estás en el sitio indicado.
                         </p>
                         <Row className="square-fitness">
                             <p className="title-square-fitness">Contenidos</p>
                             <ul>
-                                <li className="li-square-fitness">1 Los tres principios de la musculación</li>
-                                <li>2 Musculación por grupos musculares</li>
-                                <li>3 Selección de artículos de musculación</li>
+                                <li className="li-square-fitness">1 Como optimizar tus rutinas de espalda</li>
+                                <li>2 Rutina de espalda para volumen general</li>
+                                <li>3 Rutina de espalda para principiantes</li>
+                                <li>4 Rutina Casera (solo mancuernas)</li>
+                                <li>5 Rutina de 15 minutos</li>
+                                <li>6 Rutina para prioridad en dorsal inferior</li>
+                                <li>7 Rutina para prioridad de dorsal alto</li>
+                                <li>8 Entrenamiento en serie gigante*</li>
+                                <li>9 Rutina para fuerza</li>
+                                <li>10 Rutina para definición</li>
+                                <li>11 Rutina para super-congestión</li>
+                                <li>12 Rutinas de espalda especializadas</li>
+                                <li>13 Consejos avanzados para tu entrenamiento de espalda</li>
                             </ul>
                         </Row>
-                        <h3>Los tres principios de la musculación</h3>
+                        <h3>Como optimizar tus rutinas de espalda</h3>
                         <p>
-                            En el entrenamiento de la fuerza y la masa muscular hay tres principios de entrenamiento especialmente importantes y que toda persona que inicia en las pesas debe conocer.
+                            En las <strong>rutinas de espalda</strong> se deben utilizar ejercicios orientados a la <strong>amplitud</strong>, como las <strong>dominadas</strong> y los jalones pero también ejercicios más orientados a la densidad como los remos.
                         </p>
                         <p>
-                            Estos tres principios se basan en el orden correcto al realizar los ejercicios, progresión de entrenamiento y recuperación adecuada.
-                        </p>
-                        <h5>Orden de los ejercicios</h5>
-                        <p>
-                            Según este principio, los músculos grandes deben ser ejercitados antes que los pequeños. La razón es muy simple: un músculo pequeño se fatiga antes y con mayor facilidad que uno grande.
+                            En una misma rutina de espalda puedes <strong>combinar</strong> ambos tipos de ejercicios o puedes trabajar la espalda dos veces a la semana trabajando un día la amplitud y otro día la densidad, la elección es tuya según tu objetivo y capacidad de entrenamiento.
                         </p>
                         <p>
-                            Por consiguiente, si los músculos pequeños son ejercitados al principio de la sesión, pueden provocar una fatiga prematura general, impidiendo luego el óptimo rendimiento de los grupos musculares grandes.
+                            La espalda es además uno de los grupos musculares más difíciles de congestionar por los <strong>principiantes</strong>, debido a que debe realizar un <strong>intervalo completo de recorrido</strong> para estirar y contraer el músculo correctamente.
                         </p>
+                        <p>
+                            En este sentido, tener una conexión músculo-mental es muy importante para tener un entrenamiento de espalda efectivo. Para ello necesitarás tiempo, pero te aseguramos que en unos meses el entrenar tu espalda será de tus días favoritos de entrenamiento.
+                        </p>
+                        <p>
+                        <strong>Nota</strong>: <i>Los entrenamientos aquí mostrados no incluyen las series de calentamiento. A menos que se diga lo contrario, descansad de 60 a 90 segundos entre cada serie.</i>
+                        </p>
+                        <h3>Rutina de espalda para volumen general</h3>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>Ejercicio</td>
+                                    <td>Series</td>
+                                    <td>Reps</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Peso Muerto</td>
+                                    <td>5</td>
+                                    <td>12,10,8,6,6</td>
+                                </tr>
+                                <tr>
+                                    <td>Remo con barra</td>
+                                    <td>5</td>
+                                    <td>12,10,10,8,8</td>
+                                </tr>
+                                <tr>
+                                    <td>Remo sentado en polea</td>
+                                    <td>4</td>
+                                    <td>12,10,8,8</td>
+                                </tr>
+                                <tr>
+                                    <td>Jalones frontales</td>
+                                    <td>3</td>
+                                    <td>12,10,8</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <h3>Rutina de espalda para principiantes</h3>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>Ejercicio</td>
+                                    <td>Series</td>
+                                    <td>Reps</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Jalones frontales</td>
+                                    <td>4</td>
+                                    <td>12,12,10,10</td>
+                                </tr>
+                                <tr>
+                                    <td>Remo en máquina</td>
+                                    <td>3</td>
+                                    <td>10,10,10</td>
+                                </tr>
+                                <tr>
+                                    <td>Remo sentado en polea</td>
+                                    <td>3</td>
+                                    <td>12,12,12</td>
+                                </tr>
+                                <tr>
+                                    <td>Remo con mancuerna a una mano</td>
+                                    <td>3</td>
+                                    <td>10,10,10</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <h3>Rutina Casera (solo mancuernas)</h3>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>Ejercicio</td>
+                                    <td>Series</td>
+                                    <td>Reps</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Remo con mancuerna a una mantenimiento</td>
+                                    <td>4</td>
+                                    <td>20,12,10,8</td>
+                                </tr>
+                                <tr>
+                                    <td>Remo con mancuernas agarre invertido</td>
+                                    <td>4</td>
+                                    <td>20,12,10,10</td>
+                                </tr>
+                                <tr>
+                                    <td>Pull over con mancuerna</td>
+                                    <td>4</td>
+                                    <td>20,12,8,8</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <h3>Rutina de 15 minutos</h3>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>Ejercicio</td>
+                                    <td>Series</td>
+                                    <td>Reps</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Jalones al pecho agarre cerrado</td>
+                                    <td>3</td>
+                                    <td>15,10,6</td>
+                                </tr>
+                                <tr>
+                                    <td>Jalones en máquina*</td>
+                                    <td>3</td>
+                                    <td>15,10,6</td>
+                                </tr>
+                                <tr>
+                                    <td>Remo sentado en polea</td>
+                                    <td>3</td>
+                                    <td>15,10,6</td>
+                                </tr>
+                                <tr>
+                                    *No descansar más de 30 segundos entre series.
+                                    <td className="noneBorder"></td>
+                                    <td className="noneBorder"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <h3>Rutina para prioridad en dorsal inferior</h3>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>Ejercicio</td>
+                                    <td>Series</td>
+                                    <td>Reps</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Remo con barra y agarre invertido</td>
+                                    <td>4</td>
+                                    <td>12,12,10,10</td>
+                                </tr>
+                                <tr>
+                                    <td>Jalones polea con agarre cerrado</td>
+                                    <td>4</td>
+                                    <td>10,10,8,8</td>
+                                </tr>
+                                <tr>
+                                    <td>Jalones en polea con brazos estirados</td>
+                                    <td>3</td>
+                                    <td>15,12,10</td>
+                                </tr>
+                                <tr>
+                                    <td>Dominadas</td>
+                                    <td>3</td>
+                                    <td>Hasta el fallo</td>
+                                </tr> 
+                            </tbody>
+                        </table>
                     </section>
                 </Col>
                 <Aside className="aside"/>  
