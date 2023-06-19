0429a70d5d77b6dcdcb8d374b9adee21a29ee59d
sergiioss <sergiiptx@gmail.com>
2023-06-19 18:53:26 +0200

    Entrenamiento hecho

--- src/containers/Training/BigBodybuilders/BigBodybuildersTraining.jsx
+++ src/containers/Training/BigBodybuilders/BigBodybuildersTraining.jsx
@@ -1,9 +1,8 @@
 import './BigBodybuildersTraining.scss'
-import {Col, Row, Container} from 'react-bootstrap'
+import {Col, Row, Container,Button} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Image from 'react-bootstrap/Image'
 import Aside from '../../../components/Aside/Aside'
-
 
 const BigBodybuildersTraining = () => {
 
@@ -29,47 +28,121 @@ const BigBodybuildersTraining = () => {
                         <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
                         <Link className="icon-twitter" to="https://twitter.com/cambiatufisico"></Link>
                     </Row>
-                    <Row>
-                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/hombros-arnold-696x385.jpg"></Image>
+                    <Row className="images-content">
+                        <Image className="img-big-body-builders " src="https://www.cambiatufisico.com/wp-content/uploads/hombros-arnold-696x385.jpg"></Image>
                         <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/2015/10/Olympia_Collage_0-696x450.jpg"></Image>
                     </Row>
-                    <section>
-                    <Row>
-                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/hombros-arnold-696x385.jpg"></Image>
-                        
+                    <Row className="block-big-body-builders">
+                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/shawn-ray1-696x392.jpg"></Image>
+                        <Col className="data-img-body-builders">
+                            <h3>Shawn Ray, preparando la competición</h3>
+                            <p className="parrafo-img-body-builders">
+                                La <strong>musculación</strong> es una disciplina que requiere tiempo, dedicación y esfuerzo. Como su propio nombre indica, su objetivo es el <strong>desarrollo de los músculos.</strong>
+                            </p>
+                            <Button variant="outline-primary">Leer más</Button>
+                        </Col>
                     </Row>
-                        <p>
-                            La <strong>musculación</strong> es una disciplina que requiere tiempo, dedicación y esfuerzo. Como su propio nombre indica, su objetivo es el <strong>desarrollo de los músculos.</strong>
-                        </p>
-                        <p>
-                            En la actualidad, la práctica de la <strong>musculación</strong> es uno de los recursos más utilizados por todo tipo de personas ya sea para mejorar su físico, <a className="links-big-body-builders" href="">ganar masa muscular</a>, <a className="links-big-body-builders">perder peso</a> o simplemente como complemento para todo tipo de deportes.
-                        </p>
-                        <p>
-                            Te adelantamos que el éxito del proceso de musculación depende de tres pilares básicos: La dieta, el entrenamiento con rutinas de gimnasio adecuadas y el descanso. Si cuidas estos tres pilares tu éxito a la hora de alcanzar tus objetivos está asegurado.
-                        </p>
-                        <Row className="square-big-body-builders">
-                            <p className="title-square-big-body-builders">Contenidos</p>
-                            <ul>
-                                <li className="li-square-big-body-builders">1 Los tres principios de la musculación</li>
-                                <li>2 Musculación por grupos musculares</li>
-                                <li>3 Selección de artículos de musculación</li>
-                            </ul>
-                        </Row>
-                        <h3>Los tres principios de la musculación</h3>
-                        <p>
-                            En el entrenamiento de la fuerza y la masa muscular hay tres principios de entrenamiento especialmente importantes y que toda persona que inicia en las pesas debe conocer.
-                        </p>
-                        <p>
-                            Estos tres principios se basan en el orden correcto al realizar los ejercicios, progresión de entrenamiento y recuperación adecuada.
-                        </p>
-                        <h5>Orden de los ejercicios</h5>
-                        <p>
-                            Según este principio, los músculos grandes deben ser ejercitados antes que los pequeños. La razón es muy simple: un músculo pequeño se fatiga antes y con mayor facilidad que uno grande.
-                        </p>
-                        <p>
-                            Por consiguiente, si los músculos pequeños son ejercitados al principio de la sesión, pueden provocar una fatiga prematura general, impidiendo luego el óptimo rendimiento de los grupos musculares grandes.
-                        </p>
-                    </section>
+                    <Row className="block-big-body-builders">
+                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/dieta-lee-haney-696x392.jpg"></Image>
+                        <Col className="data-img-body-builders">
+                            <h3>Dieta de Lee Haney</h3>
+                            <p className="parrafo-img-body-builders">
+                                Si eres nuevo en esto del culturismo quizás no te suene el nombre de Lee Haney y pienses "¿Quien demonios es este tío?". Lee Haney obstenta, ni más ni menos, el record de Mr.Olympias ganados...
+                            </p>
+                            <Button variant="outline-primary">Leer más</Button>
+                        </Col>
+                    </Row>
+                    <Row className="block-big-body-builders">
+                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/larry-scott1-696x392.jpg"></Image>
+                        <Col className="data-img-body-builders">
+                            <h3>Larry Scott</h3>
+                            <p className="parrafo-img-body-builders">
+                                Larry Scott, "La leyenda", nació el 12 de octubre de 1938 en Blackfott (Idaho, EE UU). En su adolescencia era gimnasta, pero en la primavera del año en que cumplía 16 años comenzó su camino...
+                            </p>
+                            <Button variant="outline-primary">Leer más</Button>
+                        </Col>
+                    </Row>
+                    <Row className="block-big-body-builders">
+                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/ejercicios-arnold-696x392.jpg"></Image>
+                        <Col className="data-img-body-builders">
+                            <h3>Los ejercicios de Arnold Schwarzenegger</h3>
+                            <p className="parrafo-img-body-builders">
+                                Dentro y fuera del gimnasio, Arnold Schwarzenegger seguía su propio estilo. Un estilo que solo le hubiera dejado una imagen de éxito si no se hubiera embarcado en su desastrosa aventura política. De todas...
+                            </p>
+                            <Button variant="outline-primary">Leer más</Button>
+                        </Col>
+                    </Row>
+                    <Row className="block-big-body-builders">
+                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/jay-cutler-696x392.jpg"></Image>
+                        <Col className="data-img-body-builders">
+                            <h3>Rutina de Femorales de Jay Cutler</h3>
+                            <p className="parrafo-img-body-builders">
+                                Se conoce a Jay Cutler como una de las pocas personas que podían igualar e incluso superar el desarrollo de piernas de Ronnie Coleman, pero esto no siempre ha sido así. Siempre tuvo un...
+                            </p>
+                            <Button variant="outline-primary">Leer más</Button>
+                        </Col>
+                    </Row>
+                    <Row className="block-big-body-builders">
+                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/reg-park-696x392.jpg"></Image>
+                        <Col className="data-img-body-builders">
+                            <h3>Rutinas de pecho de culturistas míticos ✓</h3>
+                            <p className="parrafo-img-body-builders">
+                                Hoy te traemos un ranking de diez de los hombres con el mayor desarrollo pectoral de la época dorada del culturismo  (aunque hay una par de infiltrados más actuales) y revelamos cómo cada uno...
+                            </p>
+                            <Button variant="outline-primary">Leer más</Button>
+                        </Col>
+                    </Row>
+                    <Row className="block-big-body-builders">
+                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/dorian-yates.abdomen-696x392.jpg"></Image>
+                        <Col className="data-img-body-builders">
+                            <h3>Rutina y entrenamiento abdominal por Dorian Yates</h3>
+                            <p className="parrafo-img-body-builders">
+                                En este artículo, Dorian Yates, el 6 veces Mr. Olympia (y de forma consecutiva) nos muestra su visión del entrenamiento abdominal. Puede que no estéis de acuerdo en alguno de sus planteamientos pero creo que...
+                            </p>
+                            <Button variant="outline-primary">Leer más</Button>
+                        </Col>
+                    </Row>
+                    <Row className="block-big-body-builders">
+                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/piramide-invertida2-696x392.jpg"></Image>
+                        <Col className="data-img-body-builders">
+                            <h3>Entrenamiento de pirámide invertida (Por Lee Labrada)</h3>
+                            <p className="parrafo-img-body-builders">
+                                El Sistema de Pirámide recibe su nombre del hecho de hacer un ejercicio determinado aumentando el peso en cada serie hasta alcanzar el máximo kilaje previsto. En ese momento. reducimos el peso en cada...
+                            </p>
+                            <Button variant="outline-primary">Leer más</Button>
+                        </Col>
+                    </Row>
+                    <Row className="block-big-body-builders">
+                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/arnold-mejorar-gemelos-696x392.jpg"></Image>
+                        <Col className="data-img-body-builders">
+                            <h3>Mejora tus gemelos con Arnold Schwarzenegger</h3>
+                            <p className="parrafo-img-body-builders">
+                                Arnold ha escrito multitud de artículos sobre entrenamiento culturista; este es uno de ellos, publicado en una conocida revista de musculación, donde Arnold nos muestra la mejor forma (para el) de conseguir unos grandes...
+                            </p>
+                            <Button variant="outline-primary">Leer más</Button>
+                        </Col>
+                    </Row>
+                    <Row className="block-big-body-builders">
+                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/fondos-pecho-696x392.jpg.webp"></Image>
+                        <Col className="data-img-body-builders">
+                            <h3>Entrenamiento con fondos para pecho y tríceps</h3>
+                            <p className="parrafo-img-body-builders">
+                                Desde el desarrollo del gimnasio moderno, con toda su variedad de equipos de ejercicio, se hacen muy pocos entrenamientos contra resistencia en forma de calistenia o con el peso corporal.  La ausencia de movimientos con...
+                            </p>
+                            <Button variant="outline-primary">Leer más</Button>
+                        </Col>
+                    </Row>
+                    <Row className="block-big-body-builders">
+                        <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/dieta-phil-heat-696x392.jpg"></Image>
+                        <Col className="data-img-body-builders">
+                            <h3>Dieta de Phil Heath para volumen muscular</h3>
+                            <p className="parrafo-img-body-builders">
+                                Phil Heath nace en Seattle el 18 de Diciembre de 1979 y es profesional de la IFBB. Apodado "el regalo" jugo al baloncesto antes de volver al culturismo en 2002 en la universidad de Denver. Su...
+                            </p>
+                            <Button variant="outline-primary">Leer más</Button>
+                        </Col>
+                    </Row>
+                        
                 </Col>
                 <Aside className="aside"/>
             </Row>
--- src/containers/Training/Stretching/StretchingTraining.jsx
+++ src/containers/Training/Stretching/StretchingTraining.jsx
@@ -12,8 +12,7 @@ const StretchingTraining = () => {
         <Container className="stretching-exercises-training">
             <Row className="first-section-stretching">
                 <Col className="body-stretching-training" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
-                    <h1>Musculación, la guía definitiva</h1>
-                    <i>Con nuestros consejos y técnicas dominarás el arte de la musculación para cambiar tu cuerpo.</i>
+                    <h1>Estiramientos y entrenamiento de la flexibilidad en el gimnasio</h1>
                     <Row className="icons-stretching" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
                         <Link className="icon-facebook" to="https://www.facebook.com/CambiatufisicoWeb/"></Link>
                         <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
@@ -22,35 +21,50 @@ const StretchingTraining = () => {
                     <Image className="img-stretching" src="https://www.cambiatufisico.com/wp-content/uploads/estiramientos-flexibilidad-696x392.jpg.webp"></Image>
                     <section>
                         <p>
-                            La <strong>musculación</strong> es una disciplina que requiere tiempo, dedicación y esfuerzo. Como su propio nombre indica, su objetivo es el <strong>desarrollo de los músculos.</strong>
+                            Los <strong>estiramientos</strong> son indispensables en la vida cotidiana a fin de mejorar la soltura, la tonicidad y la movilidad. 
                         </p>
                         <p>
-                            En la actualidad, la práctica de la <strong>musculación</strong> es uno de los recursos más utilizados por todo tipo de personas ya sea para mejorar su físico, <a className="links-stretching" href="">ganar masa muscular</a>, <a className="links-stretching">perder peso</a> o simplemente como complemento para todo tipo de deportes.
+                            Los estiramientos no constituyen únicamente una manera de tonificar el cuerpo: ayudan a eliminar el estrés tomando conciencia del propio cuerpo.
                         </p>
                         <p>
-                            Te adelantamos que el éxito del proceso de musculación depende de tres pilares básicos: La dieta, el entrenamiento con rutinas de gimnasio adecuadas y el descanso. Si cuidas estos tres pilares tu éxito a la hora de alcanzar tus objetivos está asegurado.
+                            Los <strong>estiramientos</strong> son una gimnasia suave, sin riesgos. Nos invita a escuchar el interior de nuestro cuerpo en lugar de prestar atención tan solo a su apariencia. El cuello se libera, el vientre se aplana, el pecho se abre y las piernas se agilizan.
                         </p>
-                        <Row className="square-stretching">
-                            <p className="title-square-stretching">Contenidos</p>
-                            <ul>
-                                <li className="li-square-stretching">1 Los tres principios de la musculación</li>
-                                <li>2 Musculación por grupos musculares</li>
-                                <li>3 Selección de artículos de musculación</li>
-                            </ul>
-                        </Row>
-                        <h3>Los tres principios de la musculación</h3>
+                        <h3>Beneficios de los estiramientos</h3>
                         <p>
-                            En el entrenamiento de la fuerza y la masa muscular hay tres principios de entrenamiento especialmente importantes y que toda persona que inicia en las pesas debe conocer.
+                            El estiramiento es un gesto natural que mantiene la forma física y procura bienestar. De manera instintiva, la primera cosa que hacemos por la mañana, cuando nos despertamos, es estirarnos.
                         </p>
                         <p>
-                            Estos tres principios se basan en el orden correcto al realizar los ejercicios, progresión de entrenamiento y recuperación adecuada.
+                            Más allá de este aspecto revitalizante, los estiramientos constituyen también un buen medio para optimizar el rendimiento deportivo.
                         </p>
-                        <h5>Orden de los ejercicios</h5>
+                        <h5>Mantenimiento o aumento de la amplitud del movimiento</h5>
                         <p>
-                            Según este principio, los músculos grandes deben ser ejercitados antes que los pequeños. La razón es muy simple: un músculo pequeño se fatiga antes y con mayor facilidad que uno grande.
+                            Un gesto deportivo repetitivo puede reducir la amplitud del movimiento tensando músculos y tendones.
                         </p>
                         <p>
-                            Por consiguiente, si los músculos pequeños son ejercitados al principio de la sesión, pueden provocar una fatiga prematura general, impidiendo luego el óptimo rendimiento de los grupos musculares grandes.
+                            Una cierta tensión es indispensable, particularmente en los deportes de fuerza, pero una tensión excesiva y una amplitud reducida solo son fuente de lesiones y de disminución del rendimiento. La práctica regular de estiramientos puede prevenir este problema.
+                        </p>
+                        <p>
+                            En ciertas disciplinas, como la <strong>natación o la gimnasia</strong>, deben efectuarse ejercicios de flexibilidad regularmente, a fin de aumentar la amplitud de movimiento de una articulación cuando dicha amplitud es sinónimo de aumento del rendimiento.
+                        </p>
+                        <h5>Aumento del tono muscular</h5>
+                        <p>
+                            Los estiramientos constituyen un potente refuerzo para los músculos. Utilizando la fuerza de resistencia pasiva del músculo, el estiramiento acelera la rapidez del proceso de síntesis de las proteínas que constituyen las fibras musculares. De esta forma, nuestro cuerpo gana en tono muscular y fuerza resistencia.
+                        </p>
+                        <h5>Calentamiento antes del esfuerzo</h5>
+                        <p>
+                            Los estiramientos calienta la musculatura, los tendones y las articulaciones, con lo cual preparamos el cuerpo para un esfuerzo físico.
+                        </p>
+                        <h5>Acción antiestrés</h5>
+                        <p>
+                            Gracias a su efecto euforizante y oxigenante, los estiramientos minimizan el estrés que puede paralizar los músculos (antes de una competición, por ejemplo).
+                        </p>
+                        <h5>Prevención de lesiones</h5>
+                        <p>
+                            La mayoría de los esfuerzos musculares inducen a un aplastamiento de las articulaciones, así como a un apisonamiento de la columna vertebral. Gracias al estiramiento, es posible descomprimir la espalda y todas las articulaciones. Esta estrategia previene las lesiones acelerando la recuperación articular, tendinosa y muscular.
+                        </p>
+                        <h5>Tipos de estiramientos</h5>
+                        <p>
+                            Existen diferentes técnicas de estiramiento, pero son tres los principales métodos de probada eficacia.
                         </p>
                     </section>
                 </Col>
