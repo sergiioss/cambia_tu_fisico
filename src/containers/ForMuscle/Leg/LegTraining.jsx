951fdf283c362455c0b190f7a720b9c8092e5160
sergiioss <sergiiptx@gmail.com>
2023-06-21 13:02:03 +0200

    Lef

--- src/containers/ForMuscle/Leg/LegTraining.jsx
+++ src/containers/ForMuscle/Leg/LegTraining.jsx
@@ -19,38 +19,59 @@ const LegTraining = () => {
                         <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
                         <Link className="icon-twitter" to="https://twitter.com/cambiatufisico"></Link>
                     </Row>
-                    <Image className="img-fitness" src="https://www.cambiatufisico.com/wp-content/uploads/musculacion5-696x392.jpg"></Image>
+                    <Image className="img-fitness" src="https://www.cambiatufisico.com/wp-content/uploads/entrenamiento-piernas1-696x392.jpg"></Image>
                     <section>
                         <p>
-                            La <strong>musculación</strong> es una disciplina que requiere tiempo, dedicación y esfuerzo. Como su propio nombre indica, su objetivo es el <strong>desarrollo de los músculos.</strong>
+                            En esta sección contamos con diferentes <strong>rutinas de piernas</strong>, los entrenamientos más duros que se pueden realizar en una sala de musculación.
                         </p>
                         <p>
-                            En la actualidad, la práctica de la <strong>musculación</strong> es uno de los recursos más utilizados por todo tipo de personas ya sea para mejorar su físico, <a className="links-fitness" href="">ganar masa muscular</a>, <a className="links-fitness">perder peso</a> o simplemente como complemento para todo tipo de deportes.
+                            Hemos dividido las <strong>rutinas de piernas</strong> en diferentes apartados; por una parte tenemos aquellas rutinas que trabajan la pierna al completo, trabajando cuádriceps, femoral, glúteos, y gemelos y por otra tenemos <strong>rutinas para pierna</strong> que se centran en una parte específica de la misma.
                         </p>
                         <p>
-                            Te adelantamos que el éxito del proceso de musculación depende de tres pilares básicos: La dieta, el entrenamiento con rutinas de gimnasio adecuadas y el descanso. Si cuidas estos tres pilares tu éxito a la hora de alcanzar tus objetivos está asegurado.
-                        </p>
-                        <Row className="square-fitness">
-                            <p className="title-square-fitness">Contenidos</p>
-                            <ul>
-                                <li className="li-square-fitness">1 Los tres principios de la musculación</li>
-                                <li>2 Musculación por grupos musculares</li>
-                                <li>3 Selección de artículos de musculación</li>
-                            </ul>
-                        </Row>
-                        <h3>Los tres principios de la musculación</h3>
-                        <p>
-                            En el entrenamiento de la fuerza y la masa muscular hay tres principios de entrenamiento especialmente importantes y que toda persona que inicia en las pesas debe conocer.
+                            Iremos actualizando los diferentes apartados con nuevas rutinas de entrenamiento de forma periódica.
                         </p>
                         <p>
-                            Estos tres principios se basan en el orden correcto al realizar los ejercicios, progresión de entrenamiento y recuperación adecuada.
-                        </p>
-                        <h5>Orden de los ejercicios</h5>
-                        <p>
-                            Según este principio, los músculos grandes deben ser ejercitados antes que los pequeños. La razón es muy simple: un músculo pequeño se fatiga antes y con mayor facilidad que uno grande.
+                            Seguramente ya sabes que para trabajar las piernas hay algunos ejercicios más efectivos que otros pero ya seas hombre o mujer, joven o viejo, terrestre o extraterrestre, quieras volumen muscular o un culo que atraiga todas las miradas los ejercicios que no deberían faltar en tu rutina son las sentadillas y el peso muerto rumano. Otros ejercicios muy efectivos son las zancadas o la prensa de piernas.
                         </p>
                         <p>
-                            Por consiguiente, si los músculos pequeños son ejercitados al principio de la sesión, pueden provocar una fatiga prematura general, impidiendo luego el óptimo rendimiento de los grupos musculares grandes.
+                            Por último, recordarte la importancia de <strong>trabajar las piernas</strong> con la misma frecuencia e intensidad con las trabajas el resto de grupos musculares. El entrenar las piernas duramente te permitirá obtener mayores resultados en la parte superior de tu cuerpo ya que el cuerpo tiende a «compensarse»; además, ejercicios como la <strong>sentadilla</strong> tienen un fuerte efecto <strong>anabólico</strong> general sobre todo el cuerpo
+                        </p>
+                        <h3>Por qué debes entrenar las piernas al máximo</h3>
+                        <p>
+                            Los <strong>ejercicios de pierna</strong> pueden hacer que consigas resultados en la mitad de tiempo. Si quieres un cuerpo diez, no te olvides de las piernas.
+                        </p>
+                        <p>
+                            El temible <strong>síndrome del jilguero</strong>. Esos habitantes masculinos del gimnasio empeñados en parecerse a <strong>Johnny Bravo</strong>. Quieren brazos, pectorales, hombros y abdominales, pero se olvidan de las piernas. Piensan que con correr, la bicicleta y otros ejercicios aeróbicos, las piernas tienen suficiente. <strong>Gran error</strong>.
+                        </p>
+                        <p>
+                            Las chicas también. Quieren piernas más delgadas, y piensan que haciendo ejercicios de pierna se volverán más voluminosas. Así que se machacan en las (inútiles) máquinas para glúteos y abductores, pero no hacen ejercicios básicos de pierna. Gran error.
+                        </p>
+                        <p>
+                            ¿Y si te dijera que los <strong>ejercicios de pierna te hacen ganar músculo y perder grasa</strong> ? Recuerda que eso es lo básico, lo de tonificar y reafirmar es mentira.
+                        </p>
+                        <p>
+                            La fórmula del <strong>aumento muscular</strong> también la conocemos: hacemos un esfuerzo, las fibras musculares se “rompen” y nuestro cuerpo las reconstruye más grandes. Pero para que se produzca esta magia, tenemos que estar en <strong>estado anabólico</strong>. Por un lado tenemos que disponer de los nutrientes adecuados (proteínas e hidratos de carbono) y tiene que haber ciertas hormonas circulando por nuestra sangre, hormona de crecimiento y testosterona, y cuando comamos después de entrenar, insulina.
+                        </p>
+                        <p>
+                            En el estado anabólico, los hidratos de carbono <strong>alimentan los músculos, no las células de grasa</strong>, y las proteínas se utilizan como ladrillos para construirlos. En este estado quemas grasa después de hacer ejercicio, durante horas. Se segregan endorfinas. Hay paz en el mundo y se acaba la crisis. <strong>El estado anabólico es donde quieres estar.</strong> Y allí te llevarán tus piernas.
+                        </p>
+                        <p>
+                            Las piernas son nuestro soporte para cualquier otro ejercicio, pero sobre todo, <strong>en las piernas se encuentran los músculos más grandes del cuerpo</strong>. Cuando haces sentadillas estás reclutando mucha más fibra muscular que haciendo brazos o pectorales, consumiendo más energía y liberando más hormonas anabolizantes.
+                        </p>
+                        <p>
+                            Este efecto no se queda en las piernas, sino que se extiende. <strong>Ganar músculo en las piernas ayuda a ganar músculo en el resto del cuerpo</strong>.
+                        </p>
+                        <p>
+                            ¿Qué tienes que hacer? <strong>Sentadillas</strong>. El ejercicio más básico, el que mejores resultados da, y el más olvidado. Las sentadillas son fundamentales para mejorar tu cuerpo. Los culturistas y levantadores de pesas hacen sentadillas con una vez y media su peso corporal, como mínimo. Sin embargo, en la mayoría de los gimnasios se olvidan de ellas y en su lugar te hacen circular por cinco máquinas diferentes.
+                        </p>
+                        <p>
+                            <strong>Las sentadillas son difíciles, y necesitas tiempo para aprender a hacerlas. Si no haces bien</strong> el movimiento, mantienes una mala postura e intentas levantar demasiado peso, <strong>te puedes lesionar</strong> la espalda o las rodillas. Gravemente. En la mayoría de los gimnasios no se arriesgan, ni invierten el tiempo necesario para enseñarte. Es más seguro y más barato para el gimnasio, menos efectivo para ti.
+                        </p>
+                        <p>
+                            <strong>La sentadilla es tan importante que merece su propia entrada</strong>, pero saca una conclusión de este artículo…
+                        </p>
+                        <p>
+                            <i><strong className="center">NUNCA TE VUELVAS A SALTAR EL DÍA DE PIERNA.</strong></i>
                         </p>
                     </section>
                 </Col>
