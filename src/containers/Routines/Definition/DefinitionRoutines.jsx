9000c66390a1ef1fa4867c1e66307d9d71a85ac4
sergiioss <sergiiptx@gmail.com>
2023-06-22 17:49:11 +0200

    Definition

--- src/containers/Routines/Definition/DefinitionRoutines.jsx
+++ src/containers/Routines/Definition/DefinitionRoutines.jsx
@@ -9,49 +9,116 @@ const DefinitionRoutines = () => {
 
     return (
 
-        <Container className="fitness-training">
-            <Row className="first-section-fitness">
-                <Col className="body-fitness-training" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
-                    <h1>Musculación, la guía definitiva</h1>
-                    <i>Con nuestros consejos y técnicas dominarás el arte de la musculación para cambiar tu cuerpo.</i>
-                    <Row className="icons-fitness" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
+        <Container className="definition-routine-training">
+            <Row className="first-section-definition-routine">
+                <Col className="body-definition-routine-training" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
+                    <h1>Rutinas de Definición Muscular</h1>
+                    <i>Consigue que tus músculos sean visibles. Definición muscular extrema.</i>
+                    <Row className="icons-definition-routine" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
                         <Link className="icon-facebook" to="https://www.facebook.com/CambiatufisicoWeb/"></Link>
                         <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
                         <Link className="icon-twitter" to="https://twitter.com/cambiatufisico"></Link>
                     </Row>
-                    <Image className="img-fitness" src="https://www.cambiatufisico.com/wp-content/uploads/musculacion5-696x392.jpg"></Image>
+                    <Image className="img-definition-routine" src="https://www.cambiatufisico.com/wp-content/uploads/rutinas-definicion-muscular1-696x392.jpg"></Image>
                     <section>
                         <p>
-                            La <strong>musculación</strong> es una disciplina que requiere tiempo, dedicación y esfuerzo. Como su propio nombre indica, su objetivo es el <strong>desarrollo de los músculos.</strong>
+                            Las <strong>rutinas de definición</strong> tienen como objetivo perder la mayor parte de <strong>grasa corporal</strong> pero manteniendo la mayor cantidad de músculo posible.
                         </p>
                         <p>
-                            En la actualidad, la práctica de la <strong>musculación</strong> es uno de los recursos más utilizados por todo tipo de personas ya sea para mejorar su físico, <a className="links-fitness" href="">ganar masa muscular</a>, <a className="links-fitness">perder peso</a> o simplemente como complemento para todo tipo de deportes.
+                            Incluso en algunos casos es posible seguir ganando músculo mientras perdemos grasa, algo muy fácil de conseguir para los principiantes.
                         </p>
                         <p>
-                            Te adelantamos que el éxito del proceso de musculación depende de tres pilares básicos: La dieta, el entrenamiento con rutinas de gimnasio adecuadas y el descanso. Si cuidas estos tres pilares tu éxito a la hora de alcanzar tus objetivos está asegurado.
+                            La mayoría de las personas que realizan trabajo con pesas se pasan la mayor parte del año realizando <strong>rutinas de volumen o rutinas de fuerza. </strong>
                         </p>
-                        <Row className="square-fitness">
-                            <p className="title-square-fitness">Contenidos</p>
+                        <p>
+                            Cuando quieren marcar más los músculos o destapar esos <strong>abdominales</strong> para lucirlos en la playa suelen modificar su <strong>rutina de gimnasio</strong> habitual por las llamadas <strong>rutinas de definición.</strong>
+                        </p>
+                        <Row className="square-definition-routine">
+                            <p className="title-square-definition-routine">Contenidos</p>
                             <ul>
-                                <li className="li-square-fitness">1 Los tres principios de la musculación</li>
-                                <li>2 Musculación por grupos musculares</li>
-                                <li>3 Selección de artículos de musculación</li>
+                                <li className="li-square-definition-routine">1 Qué son las rutinas de definición muscular</li>
+                                <li>2 Rutina de definición muscular simple y efectiva</li>
+                                <li>3 La dieta para definición muscular</li>
+                                <li>4 Normas a seguir en tu rutina de definición muscular</li>
+                                <li>5 Cómo llevar tu rutina para definir al éxito</li>
+                                <li>6 Las mejores rutinas de definición</li>
+                                <li>7 Otras rutinas de definición muscular</li>
                             </ul>
                         </Row>
-                        <h3>Los tres principios de la musculación</h3>
+                        <h3>Qué son las rutinas de definición muscular</h3>
                         <p>
-                            En el entrenamiento de la fuerza y la masa muscular hay tres principios de entrenamiento especialmente importantes y que toda persona que inicia en las pesas debe conocer.
+                            Una rutina de definición muscular es un programa de entrenamiento donde el principal objetivo es <strong>reducir nuestro porcentaje de grasa corporal pero manteniendo el músculo que hemos ganado</strong> durante nuestras rutinas de fuerza y volumen.
+                        </p>
+                        <Row className="img-definition-routine">
+                            <Image src="https://www.cambiatufisico.com/wp-content/uploads/rutinas-definicion-3.jpg"></Image>
+                            <i>
+                                En las rutinas de definición se busca perder más calorías durante el entrenamiento pero no debes descuidar las cargas.
+                            </i>
+                        </Row>
+                        <p>
+                            Sabemos que es muy difícil <strong>mantener todo el músculo</strong> obtenido cuando eres un practicante natural de culturismo. La buena noticia es que aunque pierdas algo de músculo en realidad parecerá más grande y tonificado debido a la <strong>pérdida de grasa corporal</strong>.
                         </p>
                         <p>
-                            Estos tres principios se basan en el orden correcto al realizar los ejercicios, progresión de entrenamiento y recuperación adecuada.
-                        </p>
-                        <h5>Orden de los ejercicios</h5>
-                        <p>
-                            Según este principio, los músculos grandes deben ser ejercitados antes que los pequeños. La razón es muy simple: un músculo pequeño se fatiga antes y con mayor facilidad que uno grande.
+                            Como sabes el culturismo es una <strong>ilusión…</strong> una cintura estrecha con unos amplios hombros proporciona una visión más musculada que un hombre de 100 kilos cargado de músculo pero también grasa.
                         </p>
                         <p>
-                            Por consiguiente, si los músculos pequeños son ejercitados al principio de la sesión, pueden provocar una fatiga prematura general, impidiendo luego el óptimo rendimiento de los grupos musculares grandes.
+                            Un claro ejemplo de este hecho lo tenemos en el ganador del <strong>Mr.Olympia Frank Zane</strong>, que ganó el certamen más importante del culturismo pesando 80 kilos de puro músculo, proporción y simetría, batiendo a otros culturistas con 20-30 kilos más de peso
                         </p>
+                        <p>
+                            Te recordamos una vez más que el punto clave para definición muscular es la <strong>dieta</strong>.
+                        </p>
+                        <h3>Rutina de definición muscular simple y efectiva</h3>
+                        <p>
+                            En esta rutina de <strong>definición y pérdida de grasa</strong> intenta hacer cada circuito tres veces por entrenamiento. Progresa hasta 4 circuitos por entrenamiento luego de dos o tres semanas.
+                        </p>
+                        <p>
+                        Tras la <strong>octava semana</strong> modifica tu rutina de entrenamiento por alguna de las rutinas seleccionadas al final de este artículo.
+                        </p>
+                        <Row className="img-definition-routine">
+                            <Image src="https://www.cambiatufisico.com/wp-content/uploads/rutinas-definicion.jpg"></Image>
+                            <i>
+                                Un cuerpo definido no solo se consigue con mucho entrenamiento. La dieta de definición es imprescindible.
+                            </i>
+                        </Row>
+                        <p>
+                            Debes utilizar periodos de descanso de 2 minutos enteros entre cada grupo de ejercicios.
+                        </p>
+                        <p>
+                            Haremos 2 ejercicios en cada sesión de <strong></strong> usando la siguiente división:
+                        </p>
+                        <li className="margin-bottom"><strong>Día Uno: Pectorales y Espalda</strong></li>
+                        <li className="margin-bottom"><strong>Día Dos: Piernas</strong></li>
+                        <li className="margin-bottom"><strong>Día Tres: Descanso</strong></li>
+                        <li className="margin-bottom"><strong>Día Cuatro: Hombros y Brazos</strong></li>
+                        <li className="margin-bottom"><strong>Día Cinco: Descanso</strong></li>
+                        <li className="margin-bottom"><strong>Repetir Ciclo</strong></li>
+                        <p>
+                            El planteamiento de esta <strong>rutina de definición</strong> es sencillo. Los ejercicios con la misma letra se hacen en forma de <strong>superserie</strong> (más concretamente son series compuestas de tres ejercicios para el mismo grupo muscular), descansando solo 10 segundos entre cada ejercicio.
+                        </p>
+                        <p>
+                            Al final de cada superserie se descansan <strong>dos minutos.</strong>
+                        </p>
+                        <p>
+                            Posteriormente se repite cada superserie las veces necesarias. <strong>Entonces, si por ejemplo tenemos esto:</strong>
+                        </p>
+                        <li className="margin-bottom"><strong>A1</strong>. 6 (repeticiones) Press de banca con mancuernas en banco inclinado (30º) Descanso 10 segundos</li>
+                        <li className="margin-bottom"><strong>A2</strong>. 12 Press con barra en banco plano Descanso 10 segundos</li>
+                        <li className="margin-bottom"><strong>A3.</strong> 25 Aperturas con mancuerna en banco inclinado (30 º) Descanso 2 minutos</li>
+                        <h6>Repetir 3 veces</h6>
+                        <p>
+                            Realizamos el <strong>press de banca</strong>  y descansamos 10 segundos. Realizamos el press con barra y descansamos 10 segundos. Realizamos las aperturas con mancuernas en banco inclinado y ya hemos terminado la <strong>superserie</strong>, entonces descansamos dos minutos enteros. Repetimos lo mismo dos veces más.
+                        </p>
+                        <h3>La rutina de definición con efectividad 100%</h3>
+                        <h5>Día Uno: Pectorales y Espalda</h5>
+                        <li className="margin-bottom"><strong>A1</strong>. 6 (repeticiones) Press de banca con mancuernas en banco inclinado (30º). Descanso 10 segundos</li>
+                        <li className="margin-bottom"><strong>A2</strong>. 12 Press de banca. Descanso 10 segundos</li>
+                        <li className="margin-bottom"><strong>A3</strong>. 25 Aperturas con mancuerna en banco inclinado (30 º). Descanso 2 minutos</li>
+                        <h6>Repetir 3 veces</h6>
+                        <li className="margin-bottom"><strong>B1</strong>. 6 Dominadas con peso. Descanso 10 segundos</li>
+                        <li className="margin-bottom"><strong>B2</strong>. 12 Remo con barra. Descanso 10 segundos</li>
+                        <li className="margin-bottom"><strong>B3</strong>. 25 Remos en polea sentado, al cuello. Descanso 2 minutos
+                        </li>
+                        <h6>Repetir 3 veces</h6>
                     </section>
                 </Col>
                 <Aside className="aside"/>  
