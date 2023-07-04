00ae83c43d7e940b8a129f830f4bb691a46990b6
sergiioss <sergiiptx@gmail.com>
2023-07-04 23:35:03 +0200

    HealthyLiving

--- src/containers/Nutrition/HealthyLiving/HealthyLivingNutrition.jsx
+++ src/containers/Nutrition/HealthyLiving/HealthyLivingNutrition.jsx
@@ -1,5 +1,5 @@
 import './HealthyLivingNutrition.scss'
-import {Col, Row, Container} from 'react-bootstrap'
+import {Col, Row, Container, Button} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Image from 'react-bootstrap/Image'
 import Aside from '../../../components/Aside/Aside'
@@ -9,52 +9,117 @@ const HealthyLivingNutrition = () => {
 
     return (
 
-        <Container className="fitness-training">
-            <Row className="first-section-fitness">
-                <Col className="body-fitness-training" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
-                    <h1>Musculación, la guía definitiva</h1>
-                    <i>Con nuestros consejos y técnicas dominarás el arte de la musculación para cambiar tu cuerpo.</i>
-                    <Row className="icons-fitness" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
+        <Container className="healthyLiving-nutrition-training">
+            <Row className="first-section-healthyLiving-nutrition">
+                <Col className="body-healthyLiving-nutrition-training-main" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
+                    <h1>NUTRICIÓN CULTURISTA</h1>
+                    <i>
+                    <p>
+                        A veces la alimentación de los practicantes avanzados de musculación no es 100% saludable. Los culturistas llevan sus cuerpos al <strong>límite</strong> y en ocasiones tienen que introducir cambios drásticos en sus macros.
+                    </p>
+                    <p>
+                        En nuestra sección de <strong>alimentación y vida saludable</strong> descubrirás como salud y objetivos pueden ir de la mano.
+                    </p>
+                    </i>
+                    <Row className="icons-healthyLiving-nutrition" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
                         <Link className="icon-facebook" to="https://www.facebook.com/CambiatufisicoWeb/"></Link>
                         <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
                         <Link className="icon-twitter" to="https://twitter.com/cambiatufisico"></Link>
                     </Row>
-                    <Image className="img-fitness" src="https://www.cambiatufisico.com/wp-content/uploads/musculacion5-696x392.jpg"></Image>
-                    <section>
-                        <p>
-                            La <strong>musculación</strong> es una disciplina que requiere tiempo, dedicación y esfuerzo. Como su propio nombre indica, su objetivo es el <strong>desarrollo de los músculos.</strong>
-                        </p>
-                        <p>
-                            En la actualidad, la práctica de la <strong>musculación</strong> es uno de los recursos más utilizados por todo tipo de personas ya sea para mejorar su físico, <a className="links-fitness" href="">ganar masa muscular</a>, <a className="links-fitness">perder peso</a> o simplemente como complemento para todo tipo de deportes.
-                        </p>
-                        <p>
-                            Te adelantamos que el éxito del proceso de musculación depende de tres pilares básicos: La dieta, el entrenamiento con rutinas de gimnasio adecuadas y el descanso. Si cuidas estos tres pilares tu éxito a la hora de alcanzar tus objetivos está asegurado.
-                        </p>
-                        <Row className="square-fitness">
-                            <p className="title-square-fitness">Contenidos</p>
-                            <ul>
-                                <li className="li-square-fitness">1 Los tres principios de la musculación</li>
-                                <li>2 Musculación por grupos musculares</li>
-                                <li>3 Selección de artículos de musculación</li>
-                            </ul>
+                    <Row className="double-img-healthyLiving">
+                        <Row className="img-healthyLiving-nutrition">
+                            <a className="img-fluid" href="https://www.google.com/"><Image className="image-healthyLiving" src="https://www.cambiatufisico.com/wp-content/uploads/verduras-hortalizas-696x423.jpg"></Image></a>
                         </Row>
-                        <h3>Los tres principios de la musculación</h3>
-                        <p>
-                            En el entrenamiento de la fuerza y la masa muscular hay tres principios de entrenamiento especialmente importantes y que toda persona que inicia en las pesas debe conocer.
-                        </p>
-                        <p>
-                            Estos tres principios se basan en el orden correcto al realizar los ejercicios, progresión de entrenamiento y recuperación adecuada.
-                        </p>
-                        <h5>Orden de los ejercicios</h5>
-                        <p>
-                            Según este principio, los músculos grandes deben ser ejercitados antes que los pequeños. La razón es muy simple: un músculo pequeño se fatiga antes y con mayor facilidad que uno grande.
-                        </p>
-                        <p>
-                            Por consiguiente, si los músculos pequeños son ejercitados al principio de la sesión, pueden provocar una fatiga prematura general, impidiendo luego el óptimo rendimiento de los grupos musculares grandes.
-                        </p>
+                        <Row className="img-healthyLiving-nutrition">
+                            <a className="img-fluid" href="https://www.google.com/"><Image className="image-healthyLiving" src="https://www.cambiatufisico.com/wp-content/uploads/alimentos-masa-muscular-696x464.jpg"></Image></a>
+                        </Row>
+                    </Row>
+                </Col>
+                <Col className="body-healthyLiving-nutrition-training">
+                    <section>
+                        <Row className="block-big-body-builders">
+                            <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/dieta-vegetariana-adelgazar-696x465.jpg"></Image>
+                            <Col className="data-img-body-builders">
+                                <h3 className="parrafo-img-body-builders">Dieta vegetariana para adelgazar sin pasar hambre</h3>
+                                <p>
+                                    Existen muchos mitos o creencias sobre lo saludable y placentero que puede llegar a ser una dieta vegetariana. Muchos piensan que seguir una dieta vegetariana es sinónimo de pasar hambre y comer solo lechuga....
+                                </p>
+                                <Button variant="outline-primary">Leer más</Button>
+                            </Col>
+                        </Row>
+                        <Row className="block-big-body-builders">
+                            <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/comer-limpio-696x464.jpg"></Image>
+                            <Col className="data-img-body-builders">
+                                <h3 className="parrafo-img-body-builders">Comer limpio, como conseguirlo de forma fácil</h3>
+                                <p>
+                                    Es posible que hagas ejercicio desde hace tiempo, hayas invertido en ropa deportiva avanzada, en un reloj inteligente que te monitoriza hasta las veces que pestañeas e incluso puede que hayas empezado a tomar...
+                                </p>
+                                <Button variant="outline-primary">Leer más</Button>
+                            </Col>
+                        </Row>
+                        <Row className="block-big-body-builders">
+                            <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/pan-de-wasa-696x486.jpg"></Image>
+                            <Col className="data-img-body-builders">
+                                <h3 className="parrafo-img-body-builders">Pan de Wasa | Principales beneficios y Nutrientes</h3>
+                                <p>
+                                    El Pan de Wasa es un tipo de pan fino, ligero y tostado en forma de láminas no muy gruesas con diferentes tipos de semillas y granos. Su crujiente y agradable sabor lo hace muy...
+                                </p>
+                                <Button variant="outline-primary">Leer más</Button>
+                            </Col>
+                        </Row>
+                        <Row className="block-big-body-builders">
+                            <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/huevos-dieta-colesterol-696x334.jpg.webp"></Image>
+                            <Col className="data-img-body-builders">
+                                <h3 className="parrafo-img-body-builders">Huevos, un poderoso alimento para tu dieta</h3>
+                                <p>
+                                    Los huevos son un alimento muy rico en proteinas y en nutrientes. El consumo de un alimento como el huevo, tan rico en proteínas, nos puede ayudar con nuestro objetivo de ganar masa muscular. Las claras...
+                                </p>
+                                <Button variant="outline-primary">Leer más</Button>
+                            </Col>
+                        </Row>
+                        <Row className="block-big-body-builders">
+                            <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/dolor-de-espalda-696x464.jpg"></Image>
+                            <Col className="data-img-body-builders">
+                                <h3 className="parrafo-img-body-builders">Dolor de espalda, consejos para prevenirlo y curarlo</h3>
+                                <p>
+                                    Según las estadísticas las 3/4 partes de la población han padecido alguna vez un episodio de dolor de espalda intenso. El dolor de espalda aparece, en la mayoría de los casos, como el resultado de...
+                                </p>
+                                <Button variant="outline-primary">Leer más</Button>
+                            </Col>
+                        </Row>
+                        <Row className="block-big-body-builders">
+                            <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/carbohidratos-4-696x502.jpg"></Image>
+                            <Col className="data-img-body-builders">
+                                <h3 className="parrafo-img-body-builders">Carbohidratos, como dominar el combustible de tus músculos</h3>
+                                <p>
+                                    Los hidratos de carbono (HC), también denominados glúcidos, carbohidratos o, vulgarmente, azúcares, están considerados como la fuente de energía preferida por el organismo humano, tanto para las actividades físicas como mentales. Un fallo muy habitual...
+                                </p>
+                                <Button variant="outline-primary">Leer más</Button>
+                            </Col>
+                        </Row>
+                        <Row className="block-big-body-builders">
+                            <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/ginecomastia-1-696x464.jpg"></Image>
+                            <Col className="data-img-body-builders">
+                                <h3 className="parrafo-img-body-builders">Ginecomastia, lo que debes saber</h3>
+                                <p>
+                                    La ginecomastia es una acumulación de grasa, mas o menos discreta, que se localiza de forma uni o bilateral, en la zona retro areolar del pecho de las personas de sexo masculino (niños, adolescentes, adultos…). Se trata...
+                                </p>
+                                <Button variant="outline-primary">Leer más</Button>
+                            </Col>
+                        </Row>
+                        <Row className="block-big-body-builders">
+                            <Image className="img-big-body-builders" src="https://www.cambiatufisico.com/wp-content/uploads/hidratacion-muscular-696x392.jpg"></Image>
+                            <Col className="data-img-body-builders">
+                                <h3 className="parrafo-img-body-builders">La mejor hidratación para el entrenamiento</h3>
+                                <p>
+                                    El agua es, sin lugar a dudas, la sustancia nutritiva más importante, constituyendo alrededor del 60% del peso corporal total, de manera que casi podríamos afirmar que “somos agua”. Mientras que podemos aguantar sin comer...
+                                </p>
+                                <Button variant="outline-primary">Leer más</Button>
+                            </Col>
+                        </Row>
                     </section>
                 </Col>
-                <Aside className="aside"/>  
+                <Aside className="aside padding-rigth"/>  
             </Row>
         </Container>
     )
