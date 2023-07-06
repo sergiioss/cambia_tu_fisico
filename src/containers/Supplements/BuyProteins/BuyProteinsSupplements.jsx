75a9019531736ca523266992d4bfda2afce66c4e
sergiioss <sergiiptx@gmail.com>
2023-07-06 17:22:54 +0200

    Buy Proteins

--- src/containers/Supplements/BuyProteins/BuyProteinsSupplements.jsx
+++ src/containers/Supplements/BuyProteins/BuyProteinsSupplements.jsx
@@ -1,5 +1,5 @@
 import './BuyProteinsSupplements.scss'
-import {Col, Row, Container} from 'react-bootstrap'
+import {Col, Row, Container, Button} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Image from 'react-bootstrap/Image'
 import Aside from '../../../components/Aside/Aside'
@@ -9,50 +9,131 @@ const BuyProteinsSupplements = () => {
 
     return (
 
-        <Container className="fitness-training">
-            <Row className="first-section-fitness">
-                <Col className="body-fitness-training" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
-                    <h1>Musculación, la guía definitiva</h1>
-                    <i>Con nuestros consejos y técnicas dominarás el arte de la musculación para cambiar tu cuerpo.</i>
-                    <Row className="icons-fitness" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
+        <Container className="buyProteins-supplements">
+            <Row className="first-section-buyProteins">
+                <Col className="body-buyProteins-supplements" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
+                    <h1>Batidos de proteínas, que son y cuales debemos comprar</h1>
+                    <br />
+                    <Row className="icons-buyProteins" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
                         <Link className="icon-facebook" to="https://www.facebook.com/CambiatufisicoWeb/"></Link>
                         <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
                         <Link className="icon-twitter" to="https://twitter.com/cambiatufisico"></Link>
                     </Row>
-                    <Image className="img-fitness" src="https://www.cambiatufisico.com/wp-content/uploads/musculacion5-696x392.jpg"></Image>
+                    <Image className="img-buyProteins" src="https://www.cambiatufisico.com/wp-content/uploads/comprar-proteinas-696x464.jpg"></Image>
+                    <br></br>
                     <section>
                         <p>
-                            La <strong>musculación</strong> es una disciplina que requiere tiempo, dedicación y esfuerzo. Como su propio nombre indica, su objetivo es el <strong>desarrollo de los músculos.</strong>
+                            Si quieres comprar <strong>batidos proteínas</strong> estás en el lugar adecuado porque te vamos a recomendar las mejores proteínas que puedes adquirir.
                         </p>
                         <p>
-                            En la actualidad, la práctica de la <strong>musculación</strong> es uno de los recursos más utilizados por todo tipo de personas ya sea para mejorar su físico, <a className="links-fitness" href="">ganar masa muscular</a>, <a className="links-fitness">perder peso</a> o simplemente como complemento para todo tipo de deportes.
+                            Las <strong>proteínas</strong> son el pilar básico de cualquier programa de suplementación deportiva. Sus efectos para <strong>ganar músculo</strong> han sido ampliamente demostrados en diversos estudios.
                         </p>
                         <p>
-                            Te adelantamos que el éxito del proceso de musculación depende de tres pilares básicos: La dieta, el entrenamiento con rutinas de gimnasio adecuadas y el descanso. Si cuidas estos tres pilares tu éxito a la hora de alcanzar tus objetivos está asegurado.
+                           <strong> Los batidos de proteínas</strong>, además de permitir llegar con más facilidad a la cantidad de proteínas que necesita nuestro cuerpo, nos facilitan organizarnos las comidas cuando nuestra rutina familiar y laboral nos roba la mayor parte de nuestro tiempo.
                         </p>
-                        <Row className="square-fitness">
-                            <p className="title-square-fitness">Contenidos</p>
+                        <Row className="square-buyProteins">
+                            <p className="title-square-buyProteins">Contenidos</p>
                             <ul>
-                                <li className="li-square-fitness">1 Los tres principios de la musculación</li>
-                                <li>2 Musculación por grupos musculares</li>
-                                <li>3 Selección de artículos de musculación</li>
+                                <li className="li-square-buyProteins">1 Que batidos de batidos de proteínas comprar</li>
+                                <li>2 Qué son los batidos de proteína</li>
+                                <li>3 Beneficios de tomar batidos de proteínas</li>
+                                <li>4 Cómo y cuándo tomar los batidos de proteínas</li>
+                                <li>5 ¿Necesitas tomar batidos de proteínas?</li>
+                                <li>6 Nuestra recomendación para comprar proteínas</li>
                             </ul>
                         </Row>
-                        <h3>Los tres principios de la musculación</h3>
+                        <h3>Que batidos de batidos de proteínas comprar</h3>
                         <p>
-                            En el entrenamiento de la fuerza y la masa muscular hay tres principios de entrenamiento especialmente importantes y que toda persona que inicia en las pesas debe conocer.
+                            Si ya sabes lo que son los batidos de proteínas y <strong>quieres comprar unos 100%</strong> efectivos con esta selección te lo pondremos fácil.
                         </p>
                         <p>
-                            Estos tres principios se basan en el orden correcto al realizar los ejercicios, progresión de entrenamiento y recuperación adecuada.
+                            Los enlaces siguientes son <strong>solo válidos para compras en España</strong> a través de <strong>Amazon</strong> pero seguro que puedes encontrar los mismos productos en tu market local.
                         </p>
-                        <h5>Orden de los ejercicios</h5>
+                        <h5>Proteína de suero</h5>
                         <p>
-                            Según este principio, los músculos grandes deben ser ejercitados antes que los pequeños. La razón es muy simple: un músculo pequeño se fatiga antes y con mayor facilidad que uno grande.
+                            Esta proteína de marca reconocida y beneficios demostrados nunca falla.
+                        </p>
+                        <Row className="product-buyProteins">
+                            <Row className="box-product-buyProteins">
+                                <Image className="img-product-buyProteins-whey" src="https://m.media-amazon.com/images/I/61ALsbWEhIL._AC_SL1000_.jpg"></Image>
+                                <Row className="data-product-buyProteins">
+                                    <h5 className="title-product-buyProteins">
+                                        Optimum Nutrition Gold Standard 100% Whey, Proteína en Polvo para Recuperacíon y Desarrollo...
+                                    </h5>
+                                    <li className="font-size-li-buyProteins">Gold Standard 100% WHEY de Optimum Nutrition es la proteína en polvo más vendida del mundo* diseñada para respaldar tu proceso de...</li>
+                                    <li className="font-size-li-buyProteins">Proteína whey de RÁPIDA ABSORCIÓN con aislado de proteína de suero como ingrediente principal que ayuda a ganar músculo y favorece la...</li>
+                                    <li className="font-size-li-buyProteins">La fórmula de proteína whey ON está certificada por Informed Choice, contiene BCAA y GLUTAMINA naturales y es BAJA EN AZÚCAR y grasa</li>
+                                    <a href="https://www.amazon.es/amazonprime?tag=cambiatufis08-21" target="_blank"><Image src="https://www.cambiatufisico.com/wp-content/plugins/aawp/assets/img/icon-check-prime.svg"></Image></a>
+                                    <Row className="block-button-buyProteins">
+                                        <Button className="button-buyProteins" href="https://www.amazon.es/dp/B002DYIZH6?tag=cambiatufis08-21&linkCode=ogi&th=1&psc=1" target="_blank"><span className="icon-cart"></span>Comprar</Button>
+                                    </Row>
+                                </Row>
+                            </Row>
+                        </Row>
+                        <br />
+                        <Row className="box-buyProteins-parrafo">
+                            <p className="buyProteins-square">
+                            <strong className="title-red-buyProteins-square">Comprar la mejor proteína de suero</strong>
+                            </p>
+                        </Row>
+                        <h5>Proteína de Caseína</h5>
+                        <p>
+                            Liberación sostenida de aminoácidos durante toda la noche. También puedes mezclarla con tu proteína de suero concentrada y obtener una mezcla con diferentes velocidades de absorción.
                         </p>
                         <p>
-                            Por consiguiente, si los músculos pequeños son ejercitados al principio de la sesión, pueden provocar una fatiga prematura general, impidiendo luego el óptimo rendimiento de los grupos musculares grandes.
+                            No se ha encontrado el producto.
+                        </p>
+                        <Row className="box-buyProteins-parrafo">
+                            <p className="buyProteins-square">
+                            <strong className="title-red-buyProteins-square">Comprar la mejor proteína de caseina</strong>
+                            </p>
+                        </Row>
+                        <h5>Barritas de proteína de suero</h5>
+                        <p>
+                            Una de mis formas favoritas de llegar a mi ratio de proteínas son las <strong>barritas</strong>. También suponen una buena forma de aplacar las <strong>ganas de tomar dulce</strong> y son una recompensa saludable para tus duros entrenamientos.
+                        </p>
+                        <Row className="product-buyProteins">
+                            <Row className="box-product-buyProteins">
+                                <Image className="img-product-buyProteins" src="https://m.media-amazon.com/images/I/81ZY7frqFuL._AC_SL1500_.jpg"></Image>
+                                <Row className="data-product-buyProteins">
+                                    <h5 className="title-product-buyProteins">
+                                        Marca Amazon - Amfit Nutrition Barrita de proteína baja en azúcar (19,6gr proteina - 0,8gr...
+                                    </h5>
+                                    <li className="font-size-li-buyProteins">Pack de 12 (12x60g)</li>
+                                    <li className="font-size-li-buyProteins">Barrita de proteínas sabor a fondant de chocolate con una capa de caramelo sabor chocolate y cobertura sabor chocolate negro, con...</li>
+                                    <li className="font-size-li-buyProteins">La fórmula de proteína whey ON está certificada por Informed Choice, contiene BCAA y GLUTAMINA naturales y es BAJA EN AZÚCAR y grasa</li>
+                                    <a href="https://www.amazon.es/amazonprime?tag=cambiatufis08-21" target="_blank"><Image src="https://www.cambiatufisico.com/wp-content/plugins/aawp/assets/img/icon-check-prime.svg"></Image></a>
+                                    <Row className="block-button-buyProteins">
+                                        <Button className="button-buyProteins" href="https://www.amazon.es/dp/B002DYIZH6?tag=cambiatufis08-21&linkCode=ogi&th=1&psc=1" target="_blank"><span className="icon-cart"></span>Comprar</Button>
+                                    </Row>
+                                </Row>
+                            </Row>
+                        </Row>
+                        <br />
+                        <Row className="box-buyProteins-parrafo">
+                            <p className="buyProteins-square">
+                            <strong className="title-red-buyProteins-square">Comprar las mejores barritas de proteína de suero</strong>
+                            </p>
+                        </Row>
+                        <h3>Qué son los batidos de proteína</h3>
+                        <p>
+                            La proteína de leche está compuesta por un <strong>80% de caseína</strong> y un <strong>20% de suero lácteo</strong> (whey). El suero es sin duda la proteína más popular actualmente.
+                        </p>
+                        <p>
+                            Para descubrir que los batidos de proteínas no son nada mágico, ni químico, ni ilegal, observa la siguiente foto:
+                        </p>                      
+                    </section>
+                    <Image className="img-buyProteins" src="https://www.cambiatufisico.com/wp-content/uploads/2016/01/proteina-de-suero.jpg"></Image>
+                    <br></br>
+                    <section>
+                        <p>
+                            El suero lácteo posee la calidad biológica más alta, es decir, esta proteína posee los aminoácidos más asimilables por el organismo humano. 
+                        </p>
+                        <p>
+                            Existen diferentes <strong>tipos de suero lácteo,</strong> que se diferencian por su proceso de <strong>filtración</strong>. Tenemos de entrada el <strong>concentrado</strong>, que es la forma más simple, y, por lo tanto, las más barata. Las formas más avanzadas, más rápidamente digeribles son el <strong>isolado</strong> y el <strong>hidrolizado</strong>.
                         </p>
                     </section>
+                    
                 </Col>
                 <Aside className="aside"/>  
             </Row>
--- src/containers/Supplements/HowSupplements/HowSupplements.jsx
+++ src/containers/Supplements/HowSupplements/HowSupplements.jsx
@@ -9,49 +9,108 @@ const HowSupplements = () => {
 
     return (
 
-        <Container className="fitness-training">
-            <Row className="first-section-fitness">
-                <Col className="body-fitness-training" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
-                    <h1>Musculación, la guía definitiva</h1>
-                    <i>Con nuestros consejos y técnicas dominarás el arte de la musculación para cambiar tu cuerpo.</i>
-                    <Row className="icons-fitness" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
+        <Container className="howSupplements-supplements">
+            <Row className="first-section-howSupplements">
+                <Col className="body-howSupplements-supplements" xs={12} sm={12} md={8} lg={8} xl={8} xxl={8}>
+                    <h1>Suplementos dietéticos, como tomarlos</h1>
+                    <br />
+                    <Row className="icons-howSupplements" xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
                         <Link className="icon-facebook" to="https://www.facebook.com/CambiatufisicoWeb/"></Link>
                         <Link className="icon-instagram" to="https://www.instagram.com/cambiatufisico_/"></Link>
                         <Link className="icon-twitter" to="https://twitter.com/cambiatufisico"></Link>
                     </Row>
-                    <Image className="img-fitness" src="https://www.cambiatufisico.com/wp-content/uploads/musculacion5-696x392.jpg"></Image>
+                    <Image className="img-howSupplements" src="https://www.cambiatufisico.com/wp-content/uploads/suplementos-dieteticos3-696x392.jpg"></Image>
+                    <br />
                     <section>
                         <p>
-                            La <strong>musculación</strong> es una disciplina que requiere tiempo, dedicación y esfuerzo. Como su propio nombre indica, su objetivo es el <strong>desarrollo de los músculos.</strong>
+                            Gracias al avance de la tecnología y la investigación estamos viviendo una auténtica revolución en el campo de la nutrición y los <strong>suplementos dietéticos.</strong>
                         </p>
                         <p>
-                            En la actualidad, la práctica de la <strong>musculación</strong> es uno de los recursos más utilizados por todo tipo de personas ya sea para mejorar su físico, <a className="links-fitness" href="">ganar masa muscular</a>, <a className="links-fitness">perder peso</a> o simplemente como complemento para todo tipo de deportes.
+                            Hoy en día, existen <strong>deportistas</strong> que continúan con hábitos alimenticios desfasados y tiene prejuicios no solo a la hora de utilizar <strong>suplementos dietéticos</strong>, sino de cambiar su alimentación por otra más sana y adecuada para su especialidad deportiva. Un error que aunque respetable, se debe intentar no cometer.
                         </p>
                         <p>
-                            Te adelantamos que el éxito del proceso de musculación depende de tres pilares básicos: La dieta, el entrenamiento con rutinas de gimnasio adecuadas y el descanso. Si cuidas estos tres pilares tu éxito a la hora de alcanzar tus objetivos está asegurado.
+                            En la sociedad actual existen gran cantidad de prejuicios hacia los productos sintetizados <strong>artificialmente</strong> aunque paradójicamente esos prejuicios desaparecen a la hora de tomar algún <strong>medicamento</strong>.
                         </p>
-                        <Row className="square-fitness">
-                            <p className="title-square-fitness">Contenidos</p>
+                        <Row className="square-howSupplements">
+                            <p className="title-square-howSupplements">Contenidos</p>
                             <ul>
-                                <li className="li-square-fitness">1 Los tres principios de la musculación</li>
-                                <li>2 Musculación por grupos musculares</li>
-                                <li>3 Selección de artículos de musculación</li>
+                                <li className="li-square-howSupplements">1 Que son los suplementos dietéticos</li>
+                                <li>2 Los mejores suplementos dietéticos</li>
+                                <li>3 Consejos para comprar tus suplementos dietéticos</li>
+                                <li>4 Errores comunes al tomar suplementos</li>
+                                <li>5 Suplementos más importantes</li>
+                                <li>6 Otros artículos sobre suplementación</li>
                             </ul>
                         </Row>
-                        <h3>Los tres principios de la musculación</h3>
+                        <h3>Que son los suplementos dietéticos</h3>
                         <p>
-                            En el entrenamiento de la fuerza y la masa muscular hay tres principios de entrenamiento especialmente importantes y que toda persona que inicia en las pesas debe conocer.
+                            En nuestra vida diaria estamos continuamente consumiendo alimentos sintetizados artificialmente: café soluble, leches infantiles, complejos <strong>vitamínicos</strong>, cacao en polvo, aspirina efervescente, refrescos carbonatados, etc.
                         </p>
                         <p>
-                            Estos tres principios se basan en el orden correcto al realizar los ejercicios, progresión de entrenamiento y recuperación adecuada.
-                        </p>
-                        <h5>Orden de los ejercicios</h5>
-                        <p>
-                            Según este principio, los músculos grandes deben ser ejercitados antes que los pequeños. La razón es muy simple: un músculo pequeño se fatiga antes y con mayor facilidad que uno grande.
+                            Pero la evidencia esta clara. Si queremos obtener los mejores resultados y facilitarnos la dieta necesitaremos <strong>suplementarnos</strong> para sacarle el máximo rendimiento y beneficio a nuestros entrenamientos.
                         </p>
                         <p>
-                            Por consiguiente, si los músculos pequeños son ejercitados al principio de la sesión, pueden provocar una fatiga prematura general, impidiendo luego el óptimo rendimiento de los grupos musculares grandes.
+                            Esto no significa abandonar el principio de una alimentación sana y equilibrada por unos batidos, en absoluto, como su nombre indica, se trata de <strong>suplementar</strong> o complementar la alimentación, de aportar aquellos nutrientes o compuestos que con la <strong>dieta</strong> son difíciles de conseguir o que para conseguir la cantidad y calidad deseada tendríamos que hacer una cuidad selección e ingerir grandes cantidades.
                         </p>
+                        <p>
+                            En cualquier caso, en <strong>cambiatufisico</strong> siempre recomendamos que antes de comenzar a tomar suplementos, una persona debe tener al menos un nivel medio en el entrenamiento con pesas y en la elaboración de dietas.
+                        </p>
+                        <p>
+                            De otro modo no estarás aprovechando los beneficios de los suplementos y puedes comprometer tu salud por no hablar del gasto inútil de dinero.
+                        </p>
+                        <p>
+                            También recomendamos consumir suplementos de <strong>marcas conocidas</strong> , si es posible con algún <strong>estudio científico</strong> que respalde su efectividad y comprados en tiendas con una <strong>reputación</strong> fiable y contrastada, en caso contrario lo barato puede salirte caro.
+                        </p>
+                        <h3>Los mejores suplementos dietéticos</h3>
+                        <p>
+                            Cuando se descubre el mundo de los <strong>suplementos alimentarios o dietéticos</strong>, la existencia de tan inmensa oferta de productos a nuestra disposición acaba confundiéndonos.
+                        </p>
+                        <p>
+                            No suponen ningún riesgo para nuestra salud , siempre que las <strong>dosis</strong> administradas sean las recomendadas: <strong>mucha gente piensa que más es mejor y que mucho más es mucho mejor, pero aquí esto no funciona</strong>, a menos que se quieran padecer molestias gastrointestinales o sobrecargar órganos como el hígado o el riñón.
+                        </p>
+                        <p>
+                            Si bien es cierto que algunos suplementos producen beneficios científicamente probados, no sucede así con todos.
+                        </p>
+                        <p>
+                            Mediante esta sección intentaremos guiarte por este laberinto de frascos, cápsulas, ampollas, pastillas y polvos, y sugerirte suplementos dietéticos de probada <strong>seguridad y eficacia</strong> recomendándote solo lo fundamental.
+                        </p>
+                        <h5>Suplementos de proteína de suero</h5>
+                        <p>
+                            Los <strong>batidos de proteína</strong> son el pilar básico de cualquier programa de suplementación deportiva. Sus efectos anabólicos han sido ampliamente demostrados en diversos estudios.
+                        </p>
+                        <p>
+                            La proteína de leche está compuesta por un <strong>80% de caseína</strong> y un <strong>20% de suero lácteo</strong> (whey). El suero lácteo es el líquido que queda en la parte alta de los yogures. El suero es sin duda la proteína más popular actualmente.
+                        </p>
+                        <p>
+                            Efectivamente, el suero lácteo posee la calidad biológica más alta, es decir, esta proteína posee los aminoácidos más asimilables por el organismo humano.
+                        </p>    
+                        <p>
+                            Existen diferentes <strong>tipos de suero lácteo</strong>, que se diferencian por su proceso de <strong>filtración</strong>. Tenemos de entrada el <strong>concentrado</strong>, que es la forma más simple, y, por lo tanto, las más barata.  Las formas más avanzadas son el <strong>isolado</strong> y el <strong>hidrolizado</strong>.
+                        </p>   
+                        <h5>Suplementos de Creatina</h5>   
+                        <p>
+                            La creatina es probablemente el suplemento actual más eficaz para el aumento rápido de peso, músculo y fuerza.
+                        </p>   
+                        <p>
+                            Tienes una gran cantidad de información sobre el uso de la creatina en nuestra sección de suplementos de creatina.
+                        </p>  
+                        <h5>Multivitamínicos</h5>   
+                        <p>
+                            El aporte de micronutrientes continúa siendo un tema controvertido. Oficialmente, un régimen alimentario equilibrado debe aportar todas las vitaminas y minerales que el deportista necesita.
+                        </p>
+                        <p>
+                            Sin embargo, cuando se analizan las encuestas sobre el aporte nutricional de personas sedentarias, observamos que muchos de ellos no llegan ni a cubrir sus necesidades mínimas.
+                        </p>
+                        <p>
+                            Por lo tanto, es innegable que la práctica deportiva regular aumenta las pérdidas micronutricionales y la necesidad de nutrientes.
+                        </p>
+                        <p>
+                            Por regla general, los antioxidantes deberán utilizarse en forma de cóctel y no individualmente, pues sus efectos son complementarios. Por ejemplo, debido a sus propiedades liposolubles, la vitamina E se inserta en la membrana que rodea nuestras células. Y es en este punto donde ejercerá su acción antioxidante.
+                        </p>
+                        <p>
+                            En cambio, la vitamina C, hidrosoluble, actuará en la célula y en su exterior. Las vitaminas E y C tomadas aisladamente, incluso en dosis muy importantes, no podrán actuar nunca de forma tan eficaz como en una combinación de vitamina E+C.
+                        </p>
+                        <p></p>
                     </section>
                 </Col>
                 <Aside className="aside"/>  
