00901d3733a2a690b71ca9b70a74fc0fce394d4c
sergiioss <sergiiptx@gmail.com>
2023-07-19 17:19:30 +0200

    asdcaisi

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteCompression()
-  ],
-  assetsInclude:['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif'],
+  plugins: [react()],
   base:'/cambia_tu_fisico/'
 })
