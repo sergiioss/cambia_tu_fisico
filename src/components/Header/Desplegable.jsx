8356cbb987fccbc4abba182cc7f1c8144649bca4
sergiioss <sergiiptx@gmail.com>
2023-09-16 15:43:51 +0200

    Bug links deploys

--- src/components/Header/Desplegable.jsx
+++ src/components/Header/Desplegable.jsx
@@ -17,86 +17,85 @@ const Desplegable = (props) =>{
                     </li>
                     <li className={props.styles[0]}><span className={props.styles[0]}>Por Músculo</span>
                         <ul className={props.styles[1]}>
-                            <a className="desplegable" href="/abdominal">Entrenamieno de Abdominal</a>
-                            <a className="desplegable" href="/espalda">Entrenamieno de Espalda</a>
-                            <a className="desplegable" href="/biceps">Entrenamieno de Biceps</a>
-                            <a className="desplegable" href="/pecho">Entrenamieno de Pecho</a>
-                            <a className="desplegable" href="/pierna">Entrenamieno de Pierna</a>
-                            <a className="desplegable" href="/rotador">Entrenamieno de Rotador</a>
-                            <a className="desplegable" href="/hombro">Entrenamieno de Hombro</a>
-                            <a className="desplegable" href="/triceps">Entrenamieno de Triceps</a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/abdominal">Entrenamieno de Abdominal</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/espalda">Entrenamieno de Espalda</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/biceps">Entrenamieno de Biceps</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/pecho">Entrenamieno de Pecho</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/pierna">Entrenamieno de Pierna</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/rotador">Entrenamieno de Rotador</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/hombro">Entrenamieno de Hombro</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/triceps">Entrenamieno de Triceps</Link></a>
                         </ul>
                     </li>
                     <li className={props.styles[0]}><span className={props.styles[0]}>Rutinas</span>
                         <ul className={props.styles[1]}>
-                            <a className="desplegable" href="/rutinas-abdominal">Rutinas Abdominal</a>
-                            <a className="desplegable" href="/rutinas-principiantes">Rutinas Principiantes</a>
-                            <a className="desplegable" href="/rutinas-definicion">Rutinas Definicion</a>
-                            <a className="desplegable" href="/rutinas-fullbody">Rutinas Fullbdoy</a>
-                            <a className="desplegable" href="/rutinas-en-casa">Rutinas en Casa</a>
-                            <a className="desplegable" href="/rutinas-fuerza">Rutinas de Fuerza</a>
-                            <a className="desplegable" href="/rutinas-volumen">Rutinas de Volumen</a>
-                            <a className="desplegable" href="/otras-rutinas">Otras Rutinas</a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/rutinas-abdominal">Rutinas Abdominal</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/rutinas-principiantes">Rutinas Principiantes</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/rutinas-definicion">Rutinas Definicion</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/rutinas-fullbody">Rutinas Fullbdoy</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/rutinas-en-casa">Rutinas en Casa</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/rutinas-fuerza">Rutinas de Fuerza</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/rutinas-volumen">Rutinas de Volumen</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/otras-rutinas">Otras Rutinas</Link></a>
                         </ul>
                     </li>
                     <li className={props.styles[0]}><span href="#" className={props.styles[0]}>Ejercicios</span>
                         <ul className={props.styles[1]}>
-                            <a className="desplegable" href="/ejercicios-abdominal">Ejercicios de Abdominal</a>
-                            <a className="desplegable" href="/ejercicios-espalda">Ejercicios de Espalda</a>
-                            <a className="desplegable" href="/ejercicios-basicos">Ejercicios Basicos</a>
-                            <a className="desplegable" href="/ejercicios-biceps">Ejercicios de Biceps</a>
-                            <a className="desplegable" href="/ejercicios-pecho">Ejercicios de Pecho</a>
-                            <a className="desplegable" href="/ejercicios-bandas-elasticas">Ejercicios Bandas Elásticas</a>
-                            <a className="desplegable" href="/ejercicios-antebrazo">Ejercicios de Antebrazo</a>
-                            <a className="desplegable" href="/ejercicios-pierna">Ejercicios de Pierna</a>
-                            <a className="desplegable" href="/ejercicios-hombro">Ejercicios de Hombro</a>
-                            <a className="desplegable" href="/ejercicios-rotador-hombro">Ejercicios de Rotador de Hombro</a>
-                            <a className="desplegable" href="/ejercicios-trapecio">Ejercicios de Trapecio</a>
-                            <a className="desplegable" href="/ejercicios-triceps">Ejercicios de Triceps</a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/ejercicios-abdominal">Ejercicios de Abdominal</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/ejercicios-espalda">Ejercicios de Espalda</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/ejercicios-basicos">Ejercicios Basicos</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/ejercicios-biceps">Ejercicios de Biceps</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/ejercicios-pecho">Ejercicios de Pecho</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/ejercicios-bandas-elasticas">Ejercicios Bandas Elásticas</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/ejercicios-antebrazo">Ejercicios de Antebrazo</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/ejercicios-pierna">Ejercicios de Pierna</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/ejercicios-hombro">Ejercicios de Hombro</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/ejercicios-rotador-hombro">Ejercicios de Rotador de Hombro</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/ejercicios-trapecio">Ejercicios de Trapecio</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/ejerciciosEjercicios">Ejercicios de Triceps</Link></a>
                         </ul>
                     </li>
                     <li className={props.styles[0]}><span href="#" className={props.styles[0]}>Nutricion</span>
                         <ul className={props.styles[1]}>
-                            <a className="desplegable" href="/nutricion-culturista">Nutrición Culturista</a>
-                            <a className="desplegable" href="/nutricion-y-vida-saludable">Nutrición y Vida Saludable</a>
-                            <a className="desplegable" href="/nutricion-carbohidratos">Carbohidratos</a>
-                            <a className="desplegable" href="/nutricion-grasas">Grasas</a>
-                            <a className="desplegable" href="/nutricion-proteinas">Proteínas</a>
+                            <a className="desplegable"><Link  onClick={props.onClick} to="/nutricion-culturista">Nutrición Culturista</Link></a>
+                            <a className="desplegable"><Link  onClick={props.onClick} to="/nutricion-y-vida-saludable">Nutrición y Vida Saludable</Link></a>
+                            <a className="desplegable"><Link  onClick={props.onClick} to="/nutricion-carbohidratos">Carbohidratos</Link></a>
+                            <a className="desplegable"><Link  onClick={props.onClick} to="/nutricion-grasas">Grasas</Link></a>
+                            <a className="desplegable"><Link  onClick={props.onClick} to="/nutricion-proteinas">Proteínas</Link></a>
                         </ul>
                     </li>
                     <li className={props.styles[0]}><span href="#" className={props.styles[0]}>Suplementos</span>
                         <ul className={props.styles[1]}>
-                            <a className="desplegable" href="/suplementos-como-suplementarse">Como Suplementarse</a>
-                            <a className="desplegable" href="/suplementos-vitaminas-y-minerales">Vitaminas y minerales</a>
-                            <a className="desplegable" href="/suplementos-aminoacidos">Aminoacidos</a>
-                            <a className="desplegable" href="/suplementos-cafeina">Suplementos Cafeína</a>
-                            <a className="desplegable" href="/suplementos-creatina">Suplementos Creatina</a>
-                            <a className="desplegable" href="/suplementos-glutamina">Suplementos Glutamina</a>
-                            <a className="desplegable" href="/suplementos-proteinas">Suplementos Proteinas</a>
-                            <a className="desplegable" href="/suplementos-comprar-proteinas">Comprar Proteínas </a>
-                            <a className="desplegable" href="/suplementos-otros">Otros Suplementos</a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/suplementos-como-suplementarse">Como Suplementarse</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/suplementos-vitaminas-y-minerales">Vitaminas y minerales</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/suplementos-aminoacidos">Aminoacidos</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/suplementos-cafeina">Suplementos Cafeína</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/suplementos-creatina">Suplementos Creatina</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/suplementos-glutamina">Suplementos Glutamina</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/suplementos-proteinas">Suplementos Proteinas</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/suplementos-comprar-proteinas">Comprar Proteínas </Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/suplementos-otros">Otros Suplementos</Link></a>
                         </ul>
                     </li>
                     <li className={props.styles[0]}><span href="#" className={props.styles[0]}>Dietas</span>
                         <ul className={props.styles[1]}>
-                            <a className="desplegable" href="/dietas-masa-muscular">Masa Muscular</a>
-                            <a className="desplegable" href="/dietas-definicion-muscular">Definición Muscular</a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/dietas-masa-muscular">Masa Muscular</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/dietas-definicion-muscular">Definición Muscular</Link></a>
                         </ul>
                     </li>
                     <li className={props.styles[0]}><span href="#" className={props.styles[0]}>Mujer</span>
                         <ul className={props.styles[1]}>
-                            <a className="desplegable" href="/mujer-dietas-y-nutricion">Dietas y nutrición</a>
-                            <a className="desplegable" href="/mujer-rutinas">Rutinas para mujeres</a>
-                            <a className="desplegable" href="/mujer-pesas-y-entrenamientos">Pesas y Entramientos</a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/mujer-dietas-y-nutricion">Dietas y nutrición</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/mujer-pesas-y-entrenamientos">Pesas y Entramientos</Link></a>
                         </ul>
                     </li>
                     <li className={props.styles[0]}><span href="#" className={props.styles[0]}>Objetivos</span>
                         <ul className={props.styles[1]}>
-                            <a className="desplegable" href="/objetivo-quemar-grasa">Quemar Grasa</a>
-                            <a className="desplegable" href="/objetivo-ganar-masa-muscular">Ganar Masa Muscular</a>
-                            <a className="desplegable" href="/objetivo-ganar-peso">Ganar Peso</a>
-                            <a className="desplegable" href="/objetivo-perder-peso">Perder Peso</a>
-                            <a className="desplegable" href="/objetivo-definicion-muscular">Definición Muscular</a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/objetivo-quemar-grasa">Quemar Grasa</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/objetivo-ganar-masa-muscular">Ganar Masa Muscular</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/objetivo-ganar-peso">Ganar Peso</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/objetivo-perder-peso">Perder Peso</Link></a>
+                            <a className="desplegable"><Link onClick={props.onClick} to="/objetivo-definicion-muscular">Definición Muscular</Link></a>
                         </ul>
                     </li>
                 </ul>
