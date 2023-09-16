cd4f4de57a8293642fe4458bf0f23c04da76cf28
sergiioss <sergiiptx@gmail.com>
2023-09-16 15:57:57 +0200

    Arreglo links

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,8 +1,9 @@
 import './Aside.scss';
 import {Col, Row} from 'react-bootstrap';
 import {Image} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const Aside = () =>{
+const Aside = (props) =>{
 
 
     return(
@@ -12,33 +13,33 @@ const Aside = () =>{
                         <span className="title-section-words">PRODUCTOS RECOMENDADOS</span>
                         <Col className="block-img-title">
                             <Image className="img-aside" src="https://www.cambiatufisico.com/wp-content/uploads/comprar-proteinas-696x464.jpg.webp"></Image>
-                            <h3 className="title-aside"><a href="/musculacion">Batidos de proteínas, que son y cuales debemos comprar.</a></h3>
+                            <h3 className="title-aside"><Link className="desplegable" onClick={props.onClick} to="/nutricion-proteinas">Batidos de proteínas, que son y cuales debemos comprar.</Link></h3>
                         </Col>
                         <Col className="block-img-title">
                             <Image className="img-aside" src="https://www.cambiatufisico.com/wp-content/uploads/rutina-de-hipertrofia-696x453.jpg"></Image>
-                            <h3 className="title-aside"><a href="/musculacion">Batidos de proteínas, que son y cuales debemos comprar.</a></h3>
+                            <h3 className="title-aside"><Link className="desplegable" onClick={props.onClick} to="/otras-rutinas">Rutinas de hipertrofia</Link></h3>
                         </Col>
                         {/* ******************** OTRO BLOQUE ***************** */}
                         <span className="title-section-words">TENDENCIAS</span>
                         <Col className="block-img-title">
                             <Image className="img-aside" src="https://www.cambiatufisico.com/wp-content/uploads/equipamiento-para-entrenar-en-casa-696x464.jpeg"></Image>
-                            <h3 className="title-aside"><a href="/musculacion">Rutina de hipertrofia total de 3 meses</a></h3>
+                            <h3 className="title-aside"><Link className="desplegable" onClick={props.onClick} to="/rutinas-volumen">Rutina de hipertrofia total de 3 meses</Link></h3>
                         </Col>
                         <Col className="block-img-title">
                             <Image className="img-aside" src="https://www.cambiatufisico.com/wp-content/uploads/rutina-gimnasio-anual3-696x392.jpg"></Image>
-                            <h3 className="title-aside"><a href="/musculacion">Plan de entrenamiento de Gimnasio Anual</a></h3>
+                            <h3 className="title-aside"><Link className="desplegable" onClick={props.onClick} to="/rutinas-fullbody">Plan de entrenamiento de Gimnasio Anual</Link></h3>
                         </Col>
                         <Col className="block-img-title">
                             <Image className="img-aside" src="https://www.cambiatufisico.com/wp-content/uploads/rutina-volumen-arnold-696x392.jpg"></Image>
-                            <h3 className="title-aside"><a href="/musculacion">Rutina de volumen de Arnold Schwarzenegger</a></h3>
+                            <h3 className="title-aside"><Link className="desplegable" onClick={props.onClick} to="/rutinas-volumen">Rutina de volumen de Arnold Schwarzenegger</Link></h3>
                         </Col>
                         <Col className="block-img-title">
                             <Image className="img-aside" src="https://www.cambiatufisico.com/wp-content/uploads/rutinas-volumen2-696x392.jpg"></Image>
-                            <h3 className="title-aside"><a href="/musculacion">Rutinas de volumen para ganar masa muscular</a></h3>
+                            <h3 className="title-aside"><Link  className="desplegable" onClick={props.onClick} to="/dietas-masa-muscular">Rutinas de volumen para ganar masa muscular</Link></h3>
                         </Col>
                         <Col className="block-img-title">
                             <Image className="img-aside" src="https://www.cambiatufisico.com/wp-content/uploads/calculo-calorico1-696x392.jpg"></Image>
-                            <h3 className="title-aside"><a href="/musculacion">Cálculo calórico para mantenimiento ✓</a></h3>
+                            <h3 className="title-aside"><Link className="desplegable" onClick={props.onClick} to="/mujer-dietas-y-nutricion">Cálculo calórico para mantenimiento ✓</Link></h3>
                         </Col>
                     </Row>
             </Row>
